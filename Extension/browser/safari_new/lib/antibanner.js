aab08d392102415046e38f41dfbbdad097255530
mizzick <ds.kolyshev@gmail.com>
2015-10-27 12:06:57 -0100

    https://github.com/AdguardTeam/AdguardBrowserExtension/issues/16 Safari Content Blocker Extension

--- Extension/browser/safari_new/lib/antibanner.js
+++ Extension/browser/safari_new/lib/antibanner.js
@@ -51,13 +51,80 @@ AntiBannerService.prototype.getRequestFilter = function () {
         SafariContentBlocker.loadFilters(rules);
 
         // Request filter is ready
-        this.requestFilter = new RequestFilter();;
+        this.requestFilter = new RequestFilter();
+        ;
 
         // No need in dirtyRules collection anymore
         this.dirtyRules = null;
     }
 
     return this.requestFilter;
+};
+
+/**
+ * Adds domain to whitelist
+ *
+ * @param domain Domain name
+ * @returns {*}
+ */
+AntiBannerService.prototype.addWhiteListDomain = function (domain) {
+    whiteListService.addToWhiteList(domain);
+    EventNotifier.notifyListeners(EventNotifierTypes.UPDATE_WHITELIST_FILTER_RULES);
+
+    var filter = this._getFilterById(AntiBannerFiltersId.WHITE_LIST_FILTER_ID);
+    var rule = whiteListService._createWhiteListRule(domain);
+
+    EventNotifier.notifyListeners(EventNotifierTypes.ADD_RULE, filter, [rule]);
+};
+
+/**
+ * Adds list of domains to the whitelist.
+ *
+ * @param domains List of domains to add
+ */
+AntiBannerService.prototype.addWhiteListDomains = function (domains) {
+    whiteListService.addToWhiteListArray(domains);
+    EventNotifier.notifyListeners(EventNotifierTypes.UPDATE_WHITELIST_FILTER_RULES);
+
+    if (!domains) {
+        return;
+    }
+    var filter = this._getFilterById(AntiBannerFiltersId.WHITE_LIST_FILTER_ID);
+    var rules = [];
+    for (var i = 0; i < domains.length; i++) {
+        var domain = domains[i];
+        var rule = this._createWhiteListRule(domain);
+        if (rule) {
+            rules.push(rule);
+        }
+    }
+
+    EventNotifier.notifyListeners(EventNotifierTypes.ADD_RULES, filter, rules);
+};
+
+/**
+ * Removes domain from the whitelist
+ *
+ * @param domain   Domain to remove
+ */
+AntiBannerService.prototype.removeWhiteListDomain = function (domain) {
+    whiteListService.removeFromWhiteList(domain);
+    EventNotifier.notifyListeners(EventNotifierTypes.UPDATE_WHITELIST_FILTER_RULES);
+
+    var filter = this._getFilterById(AntiBannerFiltersId.WHITE_LIST_FILTER_ID);
+    var rule = whiteListService._createWhiteListRule(domain);
+    EventNotifier.notifyListeners(EventNotifierTypes.REMOVE_RULE, filter, [rule]);
+};
+
+/**
+ * Removes all domains from the whitelist
+ */
+AntiBannerService.prototype.clearWhiteListFilter = function () {
+    whiteListService.clearWhiteList();
+    EventNotifier.notifyListeners(EventNotifierTypes.UPDATE_WHITELIST_FILTER_RULES);
+
+    var filter = this._getFilterById(AntiBannerFiltersId.WHITE_LIST_FILTER_ID);
+    EventNotifier.notifyListeners(EventNotifierTypes.UPDATE_FILTER_RULES, filter, []);
 };
 
 /**
