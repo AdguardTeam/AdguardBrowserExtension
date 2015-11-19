2c5475d7f2577759ac8914b2c92afbc9104a776c
mizzick <ds.kolyshev@gmail.com>
2015-11-19 08:54:19 -0300

    https://github.com/AdguardTeam/AdguardBrowserExtension/issues/52 Removed safari_new config

--- Extension/browser/safari/lib/content-script/content-script.js
+++ Extension/browser/safari/lib/content-script/content-script.js
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 (function () {
 
 	var isSafariNew = function () {
