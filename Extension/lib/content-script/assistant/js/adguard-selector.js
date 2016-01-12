b8d4e9c3c69d0ce96f888d0fa8fae1eeda49b133
mizzick <ds.kolyshev@gmail.com>
2016-01-12 11:56:02 -0300

    https://github.com/AdguardTeam/AdguardBrowserExtension/issues/55 selector

---
+++ Extension/lib/content-script/assistant/js/adguard-selector.js
@@ -0,0 +1,76 @@
+/**
+ * This file is part of Adguard Browser Extension (https://github.com/AdguardTeam/AdguardBrowserExtension).
+ *
+ * Adguard Browser Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Adguard Browser Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Adguard selector library
+ */
+var AdguardSelectorLib = {
+
+    /**
+     * Initializes selector module.
+     * Shows default selection after setup.
+     *
+     * @param onElementSelected callback function
+     */
+    init: function (onElementSelected) {
+
+    },
+
+    /**
+     * Resets state of selector.
+     * Sets selection to default.
+     */
+    reset: function () {
+
+    },
+
+    /**
+     * Closes selector module.
+     */
+    close: function () {
+
+    },
+
+    /**
+     * Marks current selected element.
+     */
+    showSelection: function () {
+
+    },
+
+    /**
+     * Hides selection mark on current selected element.
+     */
+    hideSelection: function () {
+
+    },
+
+    /**
+     * Returns selection object info.
+     */
+    getSelection: function () {
+
+    },
+
+    /**
+     * Changes scope of current selection.
+     * Scope can be changed in parent-child direction.
+     */
+    changeScope: function () {
+
+    }
+};
