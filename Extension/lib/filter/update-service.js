7e483334355e8fbc6792c27987c88d0601f82bc5
Andrey Meshkov <ay.meshkov@gmail.com>
2016-01-19 19:28:26 +0300

    Fixed update service

--- Extension/lib/filter/update-service.js
+++ Extension/lib/filter/update-service.js
@@ -81,7 +81,7 @@ exports.ApplicationUpdateService = {
 		if (Utils.isGreaterVersion("2.0.10", runInfo.prevVersion)) {
 			methods.push(this._onUpdateRuleHitStats);
 		}
-		if (Utils.isGreaterVersion("2.1.1", runInfo.prevVersion) && Utils.isFirefoxBrowser()) {
+		if (Utils.isGreaterVersion("2.1.2", runInfo.prevVersion) && Utils.isFirefoxBrowser()) {
 			methods.push(this._onUpdateFirefoxStorage);
 		}
 
@@ -137,6 +137,7 @@ exports.ApplicationUpdateService = {
 
 					//cleanup old file
 					var removeCallback = function () {
+                        // Ignore
 					};
 					FS.removeFile(FilterStorage.FILE_PATH, removeCallback, removeCallback);
 					updateDfd.resolve();
@@ -263,7 +264,7 @@ exports.ApplicationUpdateService = {
 	 */
 	_onUpdateFirefoxStorage: function () {
 
-		Log.info('Call update to version 2.1.1');
+		Log.info('Call update to version 2.1.2');
 
 		var dfd = new Promise();
 
