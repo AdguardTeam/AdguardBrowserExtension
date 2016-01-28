83897dec54f5b43f19b44169fc61d1d2c47293e4
Andrey Meshkov <ay.meshkov@gmail.com>
2016-01-28 17:40:48 +0300

    Fixed extension settings

--- Extension/lib/utils/browser-utils.js
+++ Extension/lib/utils/browser-utils.js
@@ -114,7 +114,8 @@ var Utils = exports.Utils = {
         }
         
         if (typeof this._useOldSafariAPI == 'undefined') {
-            this._useOldSafariAPI = safari.extension.settings.getItem('useOldSafariAPI');
+            // Seems that getItem returns a string, but we'll cast it explicitly to be sure
+            this._useOldSafariAPI = (String(safari.extension.settings.getItem('useOldSafariAPI')) == 'true');
         }
         
         return !this._useOldSafariAPI;
