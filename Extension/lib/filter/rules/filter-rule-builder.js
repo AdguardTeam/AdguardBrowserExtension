430825b18a225319a20d788320fb0591ac982fad
mizzick <ds.kolyshev@gmail.com>
2016-01-28 12:20:56 -0300

    fix filter-classes cycle dependency

--- Extension/lib/filter/rules/filter-rule-builder.js
+++ Extension/lib/filter/rules/filter-rule-builder.js
@@ -1,5 +1,8 @@
 var StringUtils = require('../../../lib/utils/common').StringUtils;
 var FilterRule = require('../../../lib/filter/rules/base-filter-rule').FilterRule;
+var CssFilterRule = require('../../../lib/filter/rules/css-filter-rule').CssFilterRule;
+var UrlFilterRule = require('../../../lib/filter/rules/url-filter-rule').UrlFilterRule;
+var ScriptFilterRule = require('../../../lib/filter/rules/script-filter-rule').ScriptFilterRule;
 var Log = require('../../../lib/utils/log').Log;
 
 exports.FilterRuleBuilder = {
@@ -28,9 +31,6 @@ exports.FilterRuleBuilder = {
                 return null;
             }
 
-            var CssFilterRule = getClasses().CssFilterRule;
-            var UrlFilterRule = getClasses().UrlFilterRule;
-            var ScriptFilterRule = getClasses().ScriptFilterRule;
             if (StringUtils.startWith(ruleText, FilterRule.MASK_WHITE_LIST)) {
                 rule = new UrlFilterRule(ruleText, filterId);
             } else if (StringUtils.contains(ruleText, FilterRule.MASK_CSS_RULE) || StringUtils.contains(ruleText, FilterRule.MASK_CSS_EXCEPTION_RULE)) {
@@ -48,18 +48,3 @@ exports.FilterRuleBuilder = {
         return rule;
     }
 };
-
-/**
- * Filter classes enumeration
- */
-var classes = null;
-function getClasses() {
-    if (!classes) {
-        classes = {
-            CssFilterRule: require('../../../lib/filter/rules/css-filter-rule').CssFilterRule,
-            UrlFilterRule: require('../../../lib/filter/rules/url-filter-rule').UrlFilterRule,
-            ScriptFilterRule: require('../../../lib/filter/rules/script-filter-rule').ScriptFilterRule
-        }
-    }
-    return classes;
-}
