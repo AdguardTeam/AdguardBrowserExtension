ec28a6ac0330a9d6166726e42eb076921417c072
mizzick <ds.kolyshev@gmail.com>
2016-01-28 12:17:50 -0300

    fix filter-classes cycle dependency

--- Extension/lib/filter/rules/filter-rule-builder.js
+++ Extension/lib/filter/rules/filter-rule-builder.js
@@ -2,21 +2,6 @@ var StringUtils = require('../../../lib/utils/common').StringUtils;
 var FilterRule = require('../../../lib/filter/rules/base-filter-rule').FilterRule;
 var Log = require('../../../lib/utils/log').Log;
 
-/**
- * Filter classes enumeration
- */
-//var classes = null;
-//function getClasses() {
-//    if (!classes) {
-//        classes = {
-//            CssFilterRule: require('../../../lib/filter/rules/css-filter-rule').CssFilterRule,
-//            UrlFilterRule: require('../../../lib/filter/rules/url-filter-rule').UrlFilterRule,
-//            ScriptFilterRule: require('../../../lib/filter/rules/script-filter-rule').ScriptFilterRule
-//        }
-//    }
-//    return classes;
-//}
-
 exports.FilterRuleBuilder = {
 
     /**
@@ -43,9 +28,9 @@ exports.FilterRuleBuilder = {
                 return null;
             }
 
-            var CssFilterRule = require('../../../lib/filter/rules/css-filter-rule').CssFilterRule;
-            var UrlFilterRule = require('../../../lib/filter/rules/url-filter-rule').UrlFilterRule;
-            var ScriptFilterRule = require('../../../lib/filter/rules/script-filter-rule').ScriptFilterRule;
+            var CssFilterRule = getClasses().CssFilterRule;
+            var UrlFilterRule = getClasses().UrlFilterRule;
+            var ScriptFilterRule = getClasses().ScriptFilterRule;
             if (StringUtils.startWith(ruleText, FilterRule.MASK_WHITE_LIST)) {
                 rule = new UrlFilterRule(ruleText, filterId);
             } else if (StringUtils.contains(ruleText, FilterRule.MASK_CSS_RULE) || StringUtils.contains(ruleText, FilterRule.MASK_CSS_EXCEPTION_RULE)) {
@@ -63,3 +48,18 @@ exports.FilterRuleBuilder = {
         return rule;
     }
 };
+
+/**
+ * Filter classes enumeration
+ */
+var classes = null;
+function getClasses() {
+    if (!classes) {
+        classes = {
+            CssFilterRule: require('../../../lib/filter/rules/css-filter-rule').CssFilterRule,
+            UrlFilterRule: require('../../../lib/filter/rules/url-filter-rule').UrlFilterRule,
+            ScriptFilterRule: require('../../../lib/filter/rules/script-filter-rule').ScriptFilterRule
+        }
+    }
+    return classes;
+}
