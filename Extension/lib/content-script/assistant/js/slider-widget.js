bd2e1e439d31e2227767e3d02f4d7f5dbe556498
mizzick <ds.kolyshev@gmail.com>
2016-03-28 21:02:40 -0500

    https://github.com/AdguardTeam/AdguardBrowserExtension/issues/184 assistant layout

--- Extension/lib/content-script/assistant/js/slider-widget.js
+++ Extension/lib/content-script/assistant/js/slider-widget.js
@@ -38,10 +38,10 @@ var SliderWidget = (function (api, $) {
 
 
     var refresh = function () {
-        var handle = $(placeholder).find("." + HANDLE_CLASS);
-        handle.css('left', (value - 1) * 100 / (max - min) + "%");
+        var handle = placeholder.querySelectorAll("." + HANDLE_CLASS);
+        $(handle).css('left', (value - 1) * 100 / (max - min) + "%");
 
-        var ticks = $(placeholder).find("." + TICK_CLASS);
+        var ticks = placeholder.querySelectorAll("." + TICK_CLASS);
         for (var i = 0; i < ticks.length; i++) {
             if (i + 1 < value) {
                 $(ticks[i]).css('background-color', TICK_LEFT_COLOR);
@@ -54,19 +54,19 @@ var SliderWidget = (function (api, $) {
     var render = function () {
         $(placeholder).addClass(PLACEHOLDER_CLASS);
 
-        var handle = $('<a>', {
-            "href": "#",
-            "class": HANDLE_FULL_CLASS
-        });
-        $(placeholder).append(handle);
+        var handle = document.createElement('a');
+        handle.setAttribute('href', '#');
+        handle.setAttribute('class', HANDLE_FULL_CLASS);
+        placeholder.appendChild(handle);
 
         var count = max - min;
         var prepare = function (i) {
-            var tick = $('<div>', {"class": TICK_FULL_CLASS}).appendTo($(placeholder));
-            tick.css({
-                left: (100 / count * i) + '%',
-                width: (100 / count) + '%'
-            });
+            var tick = document.createElement('div');
+            tick.setAttribute('class', TICK_FULL_CLASS);
+            tick.style.left = (100 / count * i) + '%';
+            tick.style.width = (100 / count) + '%';
+
+            placeholder.appendChild(tick);
         };
 
         for (var i = 0; i < count; i++) {
@@ -77,7 +77,13 @@ var SliderWidget = (function (api, $) {
     };
 
     var setValue = function (v) {
-        value = v;
+        if (v < min) {
+            value = min;
+        } else if (v > max) {
+            value = max;
+        } else {
+            value = v;
+        }
 
         refresh();
 
@@ -86,46 +92,51 @@ var SliderWidget = (function (api, $) {
 
     var bindEvents = function () {
         var $placeholder = $(placeholder);
-        var $handle = $(placeholder).find("." + HANDLE_CLASS);
+        var handle = placeholder.querySelectorAll("." + HANDLE_CLASS);
+        var $handle = $(handle);
 
-        $(document).mouseup(function () {
-            $placeholder.unbind('mousemove');
-            $handle.unbind('mousemove');
+        $(document).on('mouseup', function () {
+            $placeholder.off('mousemove');
+            $handle.off('mousemove');
         });
 
         //While the ui-slider-handle is being held down reference it parent.
-        $handle.mousedown(function (e) {
+        $handle.on('mousedown', function (e) {
             e.preventDefault();
-            return $(this).parent().mousedown();
+            return this.parentNode.trigger('mousedown');
         });
 
-        var $sliderOffsetLeft = $placeholder.offset().left;
-        var $sliderWidth = $placeholder.width();
+        var rect = placeholder.getBoundingClientRect();
+        var sliderWidth = rect.width;
+        var offsetLeft = rect.left + document.body.scrollLeft;
 
         var getSliderValue = function (pageX) {
-            return Math.round((max - min) / $sliderWidth * (pageX - $sliderOffsetLeft) + min);
+            return Math.round((max - min) / sliderWidth * (pageX - offsetLeft) + min);
         };
 
         //This will prevent the slider from moving if the mouse is taken out of the
         //slider area before the mouse down has been released.
-        $placeholder.hover(function () {
-            $placeholder.bind('click', function (e) {
+        $placeholder.on('mouseenter', function () {
+            $placeholder.on('click', function (e) {
                 //calculate the correct position of the slider set the value
                 var value = getSliderValue(e.pageX);
                 setValue(value);
             });
-            $placeholder.mousedown(function () {
-                $(this).bind('mousemove', function (e) {
+            $placeholder.on('mousedown', function () {
+                $(this).on('mousemove', function (e) {
                     //calculate the correct position of the slider set the value
                     var value = getSliderValue(e.pageX);
                     setValue(value);
                 });
-            }).mouseup(function () {
-                $(this).unbind('mousemove');
             });
-        }, function () {
-            $placeholder.unbind('mousemove');
-            $placeholder.unbind('click');
+            $placeholder.on('mouseup', function () {
+                $(this).off('mousemove');
+            });
+        });
+
+        $placeholder.on('mouseleave', function () {
+            $placeholder.off('mousemove');
+            $placeholder.off('click');
         });
     };
 
@@ -147,4 +158,4 @@ var SliderWidget = (function (api, $) {
     };
 
     return api;
-})(SliderWidget || {}, $);
+})(SliderWidget || {}, balalaika);
