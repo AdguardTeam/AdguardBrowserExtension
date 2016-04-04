432a4f1376d8eda6b3d429e384ba8a411464ee37
mizzick <ds.kolyshev@gmail.com>
2016-04-04 13:20:47 -0500

    updated filters and version

--- Compiler/build.sh
+++ Compiler/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="2.2.3"
+version="2.2.4"
 
 if [[ ! ("$#" == 1) ]] || [[ ! ($1 = dev) && ! ($1 = release) && ! ($1 = beta) ]] ; then
     echo "Pass a single argument as an environment value"
