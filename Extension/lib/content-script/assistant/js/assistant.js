804033de54b0b6fce5b23279eab32c8df23c8759
mizzick <ds.kolyshev@gmail.com>
2016-04-19 09:45:11 -0500

    https://github.com/AdguardTeam/AdguardBrowserExtension/issues/184 assistant layout

--- Extension/lib/content-script/assistant/js/assistant.js
+++ Extension/lib/content-script/assistant/js/assistant.js
@@ -445,8 +445,8 @@ var AdguardAssistant = function ($) {
 			'		<div i18n="assistant_slider_explain" class="element-rule_text"></div>' +
 			'		<div class="element-rule_slider">' +
 			'			<div class="adg-slide" id="slider">' +
-			'				<div i18n="assistant_slider_min" class="adg-slide-clue-max"></div>' +
-			'				<div i18n="assistant_slider_max" class="adg-slide-clue-min"></div>' +
+			'				<div class="adg-slide-clue-max">MAX</div>' +
+			'				<div class="adg-slide-clue-min">MIN</div>' +
 			'			</div>' +
 			'		</div>' +
 			'		<div class="element-rule_more">' +
