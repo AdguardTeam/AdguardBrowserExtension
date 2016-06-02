6967eac9cbbe16d2d5d9cadbd95acc8ab4c9c9ad
Andrey Meshkov <ay.meshkov@gmail.com>
2016-06-02 15:27:47 +0300

    Minor update

--- Extension/lib/pages/export.js
+++ Extension/lib/pages/export.js
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
  */
-/* global $ */
+/* global $, contentPage */
 $(document).ready(function () {
 
     var callback = function (response) {
