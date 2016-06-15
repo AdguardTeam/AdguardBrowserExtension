0a9d513fe5627299b833c89aa1b163da0d453b08
Andrey Meshkov <ay.meshkov@gmail.com>
2016-06-15 23:32:04 +0200

    https://github.com/AdguardTeam/AdguardBrowserExtension/issues/270

--- Compiler/build.sh
+++ Compiler/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="2.3.8"
+version="2.3.9"
 
 if [[ ! ("$#" == 1) ]] || [[ ! ($1 = dev) && ! ($1 = release) && ! ($1 = beta) ]] ; then
     echo "Pass a single argument as an environment value"
