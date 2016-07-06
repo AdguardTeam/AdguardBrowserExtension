5d9e025b65c42ad258a57e9ba673c7e386be2489
mizzick <ds.kolyshev@gmail.com>
2016-07-06 22:39:30 +0530

    #5 chrome devtools integration

--- Extension/lib/pages/devtools-elements-sidebar.js
+++ Extension/lib/pages/devtools-elements-sidebar.js
@@ -163,12 +163,11 @@ var browser = window.browser || chrome;
                 checked = '" checked="true"';
             }
 
-            //TODO: xss
             var el = $(
                 '<li class="parent">'
                 + '<input class="enabled-button attribute-check-box" type="checkbox" id="' + 'attribute-check-box-' + attributeName + checked + '">'
                 + '<span class="webkit-css-property">' + attributeName + '</span>: '
-                + '<span class="value attribute-check-box-value">' + attributeValue + '</span>'
+                + '<span class="value attribute-check-box-value">' + encodeURI(attributeValue) + '</span>'
                 + '</li>');
             return el.get(0);
         };
@@ -215,7 +214,7 @@ var browser = window.browser || chrome;
                         if (attr) {
                             if (attrName == 'class') {
                                 var className = el.parentNode.querySelector('.attribute-check-box-value').innerText;
-                                selectedClasses.push('.' + className);
+                                selectedClasses.push('.' + decodeURI(className));
                             } else {
                                 attributesSelector += '[' + attr.name + '="' + attr.value + '"]';
                             }
