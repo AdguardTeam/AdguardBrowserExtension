4aac4159270f9013751a1a07467a60d1b87b6c5a
mizzick <ds.kolyshev@gmail.com>
2016-08-16 23:19:46 +0300

    #321 extended selector

--- Extension/lib/css/extended-css.js
+++ Extension/lib/css/extended-css.js
@@ -15,6 +15,12 @@
  * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Extended css class
+ *
+ * @param styleSheet
+ * @constructor
+ */
 var ExtendedCss = function (styleSheet) {
     var rules = [];
     var affectedElements = [];
@@ -27,42 +33,80 @@ var ExtendedCss = function (styleSheet) {
      */
     var parse = function (styleSheet) {
 
-        // TODO: Parse and create ExtendedSelector objects here
-        return [
-            {selectors: 'blahblah, blahblah', style: {display: 'none!important;'}},
-            {selectors: 'blahblah2, blahblah2', style: {display: 'none!important;'}}
-        ];
+        var rulesForCssText = function (styleContent) {
+            var doc = document.implementation.createHTMLDocument(""),
+                styleElement = document.createElement("style");
+
+            styleElement.textContent = styleContent;
+            // the style will only be parsed once it is added to a document
+            doc.body.appendChild(styleElement);
+
+            return styleElement.sheet.cssRules;
+        };
+
+        var rules = rulesForCssText(styleSheet);
+
+        var result = [];
+        for (var i = 0; i < rules.length; i++) {
+            var rule = rules[i];
+            var selector = rule.selectorText;
+            if (selector && (selector.indexOf('-ext-has') >= 0 || selector.indexOf('-ext-contains') >= 0)) {
+                result.push({
+                    selector: new ExtendedSelector(rule.selectorText),
+                    style: {display: 'none! important;'}
+                });
+            }
+        }
+
+        return result;
     };
 
     /**
-     * Applies filtering rules
-     *
-     * @param rules Rules to apply
-     */
+     * Applies filtering rules
+     *
+     * @param rules Rules to apply
+     */
     var applyRules = function (rules) {
-        // TODO: Apply specified rules
+        for (var i = 0; i < rules.length; i++) {
+            var rule = rules[i];
+
+            var selector = rule.selector;
+            var elements = selector.querySelectorAll();
+
+            var iElements = elements.length;
+            while (iElements--) {
+                var el = elements[iElements];
+                var originalStyle = el.style;
+
+                el.style = rule.style;
+
+                affectedElements.push({
+                    element: el,
+                    matchedSelector: selector,
+                    originalStyle: originalStyle
+                });
+            }
+        }
     };
 
     /**
-     * Checks that affected elements are still matching our selectors
-     */
+     * Checks that affected elements are still matching our selectors
+     */
     var checkAffectedElements = function () {
         var iElements = affectedElements.length;
         while (iElements--) {
-            var obj = affectedElements[i];
-            // TODO: Check if element still matches one of the selectors
+            var obj = affectedElements[iElements];
             if (obj.matchedSelector.matches(obj.element)) {
                 // We're good, do nothing (or maybe apply it again?)
             } else {
-                // TODO: Remove our styles (something like that)
                 obj.element.style = obj.originalStyle;
             }
         }
     };
 
     /**
-     * Observe changes
-     */
+     * Observe changes
+     */
     var observe = function () {
         if (domObserver) {
             // Observer is already here
@@ -88,8 +132,10 @@ var ExtendedCss = function (styleSheet) {
     };
 
     /**
-     * Called on any DOM change, we should examine extended CSS again
-     */
+     * Called on any DOM change, we should examine extended CSS again
+     *
+     * @param mutations
+     */
     var onDomChanged = function (mutations) {
 
         if (mutations.length) {
@@ -99,21 +145,23 @@ var ExtendedCss = function (styleSheet) {
     };
 
     /**
-     * Applies extended CSS rules
-     */
+     * Applies extended CSS rules
+     */
     var apply = function () {
         applyRules(rules);
         observe();
     };
 
     /**
-     * Disposes ExtendedCss and removes our styles from matched elements
-     */
+     * Disposes ExtendedCss and removes our styles from matched elements
+     */
     var dispose = function () {
-        observer.disconnect();
+        domObserver.disconnect();
         var iElements = affectedElements.length;
         while (iElements--) {
-            // TODO: removeStyle
+            var obj = affectedElements[iElements];
+
+            obj.element.style = obj.originalStyle;
         }
     };
 
