00f07d19a2e30083f982bde98d2893157b314d1f
Andrey Meshkov <ay.meshkov@gmail.com>
2016-08-25 15:47:36 +0300

    updated extended css library

--- Extension/lib/libs/extended-css.js
+++ Extension/lib/libs/extended-css.js
@@ -145,310 +145,6 @@ var DomObserver = (function() { // jshint ignore:line
         };
     };
 })();
-/* global Sizzle, console, StylePropertyMatcher */
-
-/**
- * Extended selector class.
- * The purpose of this class is to add support for extended pseudo-classes:
- * https://github.com/AdguardTeam/AdguardBrowserExtension/issues/321
- * https://github.com/AdguardTeam/AdguardBrowserExtension/issues/322
- * <br/>
- * Please note, that instead of using the pseudo-classes we use a bit different syntax.
- * This saves us from backward compatibility issues.
- * <br/>
- * Extended selection capabilities:<br/>
- * [-ext-has="selector"] - the same as :has() pseudo class from CSS4 specification
- * [-ext-contains="string"] - allows to select elements containing specified string
- * [-ext-matches-css="|background-image: url(data:*)"]
- */
-var ExtendedSelector = (function () { // jshint ignore:line
-
-    // Add :matches-css-*() support
-    StylePropertyMatcher.extendSizzle(Sizzle);    
-
-    /**
-     * Complex replacement function. 
-     * Unescapes quote characters inside of an extended selector.
-     * 
-     * @param match     Whole matched string
-     * @param name      Group 1
-     * @param quoteChar Group 2
-     * @param value     Group 3
-     */
-    var evaluateMatch = function(match, name, quoteChar, value) { 
-        // Unescape quotes
-        var re = new RegExp("([^\\\\]|^)\\\\" + quoteChar, "g");
-        value = value.replace(re, "$1" + quoteChar);
-        return ":" + name + "(" + value + ")";
-    };
-
-    /**
-     * Prepares specified selector and compiles it with the Sizzle engine.
-     * Preparation means transforming [-ext-*=""] attributes to pseudo classes.
-     * 
-     * @param selectorText Selector text
-     */
-    var prepareSelector = function(selectorText) {
-        try {
-            // Prepare selector to be compiled with Sizzle
-            // Which means transform [-ext-*=""] attributes to pseudo classes
-            var re = /\[-ext-([a-z-_]+)=(["'])((?:(?=(\\?))\4.)*?)\2\]/g;
-            var str = selectorText.replace(re, evaluateMatch);
-            
-            // Compiles and validates selector
-            // Compilation in Sizzle means that selector will be saved to an inner cache and then reused
-            Sizzle.compile(str);
-            return str;
-        } catch (ex) {
-            if (typeof console !== 'undefined' && console.error) {
-                console.error('Extended selector is invalid: ' + selectorText);
-            }
-            return null;
-        }
-    };
-
-    // Constructor
-    return function(selectorText) {
-        var compiledSelector = prepareSelector(selectorText);
-
-        // EXPOSE
-        this.compiledSelector = compiledSelector;
-        this.selectorText = selectorText;
-
-        /**
-         * Selects all DOM nodes matching this selector.
-         */
-        this.querySelectorAll = function() {
-            return Sizzle(compiledSelector); // jshint ignore:line
-        };
-
-        /**
-         * Checks if the specified element matches this selector
-         */
-        this.matches = function(element) {
-            return Sizzle.matchesSelector(element, compiledSelector);
-        };
-    };
-})();
-/* global CssParser, DomObserver, ExtendedSelector, Utils */
-
-/**
- * Extended css class
- *
- * @param styleSheet
- * @constructor
- */
-var ExtendedCss = function (styleSheet) { // jshint ignore:line
-    var rules = [];
-    var affectedElements = [];
-    var domObserver;
-
-    /**
-     * Parses specified styleSheet in a number of rule objects
-     *
-     * @param styleSheet String with the stylesheet
-     */ 
-    var parse = function (styleSheet) {
-
-        var result = [];
-        var cssRules = CssParser.parseCss(styleSheet);
-        var iCssRules = cssRules.length;
-        while (iCssRules--) {
-            var cssRule = cssRules[iCssRules];
-
-            var ruleObject = Object.create(null);
-            ruleObject.selector = new ExtendedSelector(cssRule.selectors);
-            ruleObject.style = cssRule.style;
-            result.push(ruleObject);
-        }
-
-        return result;
-    };
-
-    /**
-     * Checks if specified element is already in the affectedElements collection
-     * 
-     * @param element
-     */
-    var checkElementIsAffected = function(element) {
-        var iAffectedElements = affectedElements.length;
-        while (iAffectedElements--) {
-            if (affectedElements[iAffectedElements].element === element) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    /**
-     * Applies style to an element
-     * 
-     * @param element DOM node
-     * @param style   Plain JS object with styles
-     */
-    var applyStyle = function(element, style) {
-        
-        for (var prop in style) {
-            
-            // Old browsers require property names to be in lowerCamelCase:
-            // https://github.com/AdguardTeam/ExtendedCss/issues/5
-            var propertyName = Utils.lowerCamelCase(prop);
-            
-            // Apply this style only to existing properties
-            // We can't use hasOwnProperty here (does not work in FF)
-            if (typeof element.style[propertyName] !== "undefined") {
-                var value = style[prop];
-
-                // First we should remove !important attribute (or it won't be applied')
-                value = value.split("!")[0].trim();
-                element.style[propertyName] = value;
-            }
-        }
-    };
-
-    /**
-     * Reverts style for the affected object
-     */
-    var revertStyle = function(affectedElement) {
-        affectedElement.element.style.cssText = affectedElement.originalStyle;
-    };
-
-    /**
-     * Applies specified rule and returns list of elements affected
-     * 
-     * @param rule Rule to apply
-     * @returns List of elements affected by this rule
-     */
-    var applyRule = function(rule) {
-        var selector = rule.selector;
-        var elements = selector.querySelectorAll();
-
-        var iElements = elements.length;
-        while (iElements--) {
-            var element = elements[iElements];
-            if (checkElementIsAffected(element)) {
-                // We have already applied style to this element
-                // Let's re-apply style to it
-                applyStyle(element, rule.style);
-            } else {
-                // Applying style first time
-                var originalStyle = element.style.cssText;
-                applyStyle(element, rule.style);
-
-                affectedElements.push({
-                    element: element,
-                    rule: rule,
-                    originalStyle: originalStyle
-                });
-            }
-        }
-
-        return elements;
-    };
-
-    /**
-     * Applies filtering rules
-     *
-     * @param rules Rules to apply
-     */
-    var applyRules = function (rules) {
-
-        var elementsIndex = [];
-        var iRules = rules.length;
-        while (iRules--) {
-            var rule = rules[iRules];
-            var elements = applyRule(rule);
-            elementsIndex = elementsIndex.concat(elements);
-        }
-
-        // Now revert styles for elements which are no more affected
-        var iAffectedElements = affectedElements.length;
-        while (iAffectedElements--) {
-            var obj = affectedElements[iAffectedElements];
-            if (elementsIndex.indexOf(obj.element) === -1) {
-                // Time to revert style
-                revertStyle(obj);
-                affectedElements.splice(iAffectedElements, 1);                
-            }
-        }
-    };
-
-    var domChanged = false;
-    
-    /**
-     * Called on any DOM change, we should examine extended CSS again.
-     */
-    var handleDomChange = function() {
-        if (!domChanged) {
-            return;
-        }
-
-        domChanged = false;
-        applyRules(rules);
-    };
-
-    /**
-     * Throttles handleDomChange function
-     */
-    var handleDomChangeAsync = function() {
-        
-        if (domChanged) {
-            return;
-        }
-        domChanged = true;
-        setTimeout(handleDomChange, 10);
-    };
-
-    /**
-     * Observe changes
-     */
-    var observe = function () {
-        if (domObserver) {
-            // Observer is already here
-            return;
-        }
-
-        // Handle dynamically added elements
-        domObserver = new DomObserver(handleDomChangeAsync);
-        domObserver.observe();
-    };
-
-    /**
-     * Applies extended CSS rules
-     */
-    var apply = function () {
-        applyRules(rules);
-        observe();
-
-        if (document.readyState !== "complete") {
-            document.addEventListener("DOMContentLoaded", function() {
-                applyRules(rules);
-            });
-        }
-    };
-
-    /**
-     * Disposes ExtendedCss and removes our styles from matched elements
-     */
-    var dispose = function () {
-        domObserver.dispose();
-        var iElements = affectedElements.length;
-        while (iElements--) {
-            var obj = affectedElements[iElements];
-            revertStyle(obj);
-        }
-    };
-
-    // First of all parse the stylesheet
-    rules = parse(styleSheet);
-
-    // EXPOSE
-    this.dispose = dispose;
-    this.apply = apply;
-    this.getAffectedElements = function() {
-        return affectedElements;
-    };
-};
 /**
  * Helper class for creating regular expression from a simple wildcard-syntax used in basic filters
  */
@@ -3030,6 +2726,307 @@ var Utils = (function () { // jshint ignore:line
         lowerCamelCase: lowerCamelCase
     };
 })();
+/* global Sizzle, console, StylePropertyMatcher */
+
+/**
+ * Extended selector class.
+ * The purpose of this class is to add support for extended pseudo-classes:
+ * https://github.com/AdguardTeam/AdguardBrowserExtension/issues/321
+ * https://github.com/AdguardTeam/AdguardBrowserExtension/issues/322
+ * <br/>
+ * Please note, that instead of using the pseudo-classes we use a bit different syntax.
+ * This saves us from backward compatibility issues.
+ * <br/>
+ * Extended selection capabilities:<br/>
+ * [-ext-has="selector"] - the same as :has() pseudo class from CSS4 specification
+ * [-ext-contains="string"] - allows to select elements containing specified string
+ * [-ext-matches-css="|background-image: url(data:*)"]
+ */
+var ExtendedSelector = (function () { // jshint ignore:line
+
+    // Add :matches-css-*() support
+    StylePropertyMatcher.extendSizzle(Sizzle);    
+
+    /**
+     * Complex replacement function. 
+     * Unescapes quote characters inside of an extended selector.
+     * 
+     * @param match     Whole matched string
+     * @param name      Group 1
+     * @param quoteChar Group 2
+     * @param value     Group 3
+     */
+    var evaluateMatch = function(match, name, quoteChar, value) { 
+        // Unescape quotes
+        var re = new RegExp("([^\\\\]|^)\\\\" + quoteChar, "g");
+        value = value.replace(re, "$1" + quoteChar);
+        return ":" + name + "(" + value + ")";
+    };
+
+    /**
+     * Prepares specified selector and compiles it with the Sizzle engine.
+     * Preparation means transforming [-ext-*=""] attributes to pseudo classes.
+     * 
+     * @param selectorText Selector text
+     */
+    var prepareSelector = function(selectorText) {
+        try {
+            // Prepare selector to be compiled with Sizzle
+            // Which means transform [-ext-*=""] attributes to pseudo classes
+            var re = /\[-ext-([a-z-_]+)=(["'])((?:(?=(\\?))\4.)*?)\2\]/g;
+            var str = selectorText.replace(re, evaluateMatch);
+            
+            // Compiles and validates selector
+            // Compilation in Sizzle means that selector will be saved to an inner cache and then reused
+            Sizzle.compile(str);
+            return str;
+        } catch (ex) {
+            if (typeof console !== 'undefined' && console.error) {
+                console.error('Extended selector is invalid: ' + selectorText);
+            }
+            return null;
+        }
+    };
+
+    // Constructor
+    return function(selectorText) {
+        var compiledSelector = prepareSelector(selectorText);
+
+        // EXPOSE
+        this.compiledSelector = compiledSelector;
+        this.selectorText = selectorText;
+
+        /**
+         * Selects all DOM nodes matching this selector.
+         */
+        this.querySelectorAll = function() {
+            return Sizzle(compiledSelector); // jshint ignore:line
+        };
+
+        /**
+         * Checks if the specified element matches this selector
+         */
+        this.matches = function(element) {
+            return Sizzle.matchesSelector(element, compiledSelector);
+        };
+    };
+})();
+/* global CssParser, DomObserver, ExtendedSelector */
+
+/**
+ * Extended css class
+ *
+ * @param styleSheet
+ * @constructor
+ */
+var ExtendedCss = function (styleSheet) { // jshint ignore:line
+    var rules = [];
+    var affectedElements = [];
+    var domObserver;
+
+    /**
+     * Parses specified styleSheet in a number of rule objects
+     *
+     * @param styleSheet String with the stylesheet
+     */ 
+    var parse = function (styleSheet) {
+
+        var result = [];
+        var cssRules = CssParser.parseCss(styleSheet);
+        var iCssRules = cssRules.length;
+        while (iCssRules--) {
+            var cssRule = cssRules[iCssRules];
+
+            var ruleObject = Object.create(null);
+            ruleObject.selector = new ExtendedSelector(cssRule.selectors);
+            ruleObject.style = cssRule.style;
+            result.push(ruleObject);
+        }
+
+        return result;
+    };
+
+    /**
+     * Checks if specified element is already in the affectedElements collection
+     * 
+     * @param element
+     */
+    var checkElementIsAffected = function(element) {
+        var iAffectedElements = affectedElements.length;
+        while (iAffectedElements--) {
+            if (affectedElements[iAffectedElements].element === element) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    /**
+     * Applies style to an element
+     * 
+     * @param element DOM node
+     * @param style   Plain JS object with styles
+     */
+    var applyStyle = function(element, style) {
+        var currentStyle = CssParser.parseStyle(element.style.cssText);
+
+        var prop;        
+        var cssText = "";
+        for (prop in style) {
+            currentStyle[prop] = style[prop];
+        }
+        for (prop in currentStyle) {
+            cssText += prop;
+            cssText += ": ";
+            cssText += currentStyle[prop];
+            cssText += "; ";
+        }        
+        element.style.cssText = cssText;
+    };
+
+    /**
+     * Reverts style for the affected object
+     */
+    var revertStyle = function(affectedElement) {
+        affectedElement.element.style.cssText = affectedElement.originalStyle;
+    };
+
+    /**
+     * Applies specified rule and returns list of elements affected
+     * 
+     * @param rule Rule to apply
+     * @returns List of elements affected by this rule
+     */
+    var applyRule = function(rule) {
+        var selector = rule.selector;
+        var elements = selector.querySelectorAll();
+
+        var iElements = elements.length;
+        while (iElements--) {
+            var element = elements[iElements];
+            if (checkElementIsAffected(element)) {
+                // We have already applied style to this element
+                // Let's re-apply style to it
+                applyStyle(element, rule.style);
+            } else {
+                // Applying style first time
+                var originalStyle = element.style.cssText;
+                applyStyle(element, rule.style);
+
+                affectedElements.push({
+                    element: element,
+                    rule: rule,
+                    originalStyle: originalStyle
+                });
+            }
+        }
+
+        return elements;
+    };
+
+    /**
+     * Applies filtering rules
+     *
+     * @param rules Rules to apply
+     */
+    var applyRules = function (rules) {
+
+        var elementsIndex = [];
+        var iRules = rules.length;
+        while (iRules--) {
+            var rule = rules[iRules];
+            var elements = applyRule(rule);
+            elementsIndex = elementsIndex.concat(elements);
+        }
+
+        // Now revert styles for elements which are no more affected
+        var iAffectedElements = affectedElements.length;
+        while (iAffectedElements--) {
+            var obj = affectedElements[iAffectedElements];
+            if (elementsIndex.indexOf(obj.element) === -1) {
+                // Time to revert style
+                revertStyle(obj);
+                affectedElements.splice(iAffectedElements, 1);                
+            }
+        }
+    };
+
+    var domChanged = false;
+    
+    /**
+     * Called on any DOM change, we should examine extended CSS again.
+     */
+    var handleDomChange = function() {
+        if (!domChanged) {
+            return;
+        }
+
+        domChanged = false;
+        applyRules(rules);
+    };
+
+    /**
+     * Throttles handleDomChange function
+     */
+    var handleDomChangeAsync = function() {
+        
+        if (domChanged) {
+            return;
+        }
+        domChanged = true;
+        setTimeout(handleDomChange, 10);
+    };
+
+    /**
+     * Observe changes
+     */
+    var observe = function () {
+        if (domObserver) {
+            // Observer is already here
+            return;
+        }
+
+        // Handle dynamically added elements
+        domObserver = new DomObserver(handleDomChangeAsync);
+        domObserver.observe();
+    };
+
+    /**
+     * Applies extended CSS rules
+     */
+    var apply = function () {
+        applyRules(rules);
+        observe();
+
+        if (document.readyState !== "complete") {
+            document.addEventListener("DOMContentLoaded", function() {
+                applyRules(rules);
+            });
+        }
+    };
+
+    /**
+     * Disposes ExtendedCss and removes our styles from matched elements
+     */
+    var dispose = function () {
+        domObserver.dispose();
+        var iElements = affectedElements.length;
+        while (iElements--) {
+            var obj = affectedElements[iElements];
+            revertStyle(obj);
+        }
+    };
+
+    // First of all parse the stylesheet
+    rules = parse(styleSheet);
+
+    // EXPOSE
+    this.dispose = dispose;
+    this.apply = apply;
+    this.getAffectedElements = function() {
+        return affectedElements;
+    };
+};
 // EXPOSE
 return ExtendedCss;
 })(window);
