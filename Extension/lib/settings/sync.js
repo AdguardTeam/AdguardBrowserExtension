09f29d1f127ba5157e938eeeb755f5de604466f5
mizzick <ds.kolyshev@gmail.com>
2016-09-04 22:12:29 +0530

    #315 sync settings

--- Extension/lib/settings/sync.js
+++ Extension/lib/settings/sync.js
@@ -1,32 +1,61 @@
 /**
-* This file is part of Adguard Browser Extension (https://github.com/AdguardTeam/AdguardBrowserExtension).
-*
-* Adguard Browser Extension is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Adguard Browser Extension is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of Adguard Browser Extension (https://github.com/AdguardTeam/AdguardBrowserExtension).
+ *
+ * Adguard Browser Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Adguard Browser Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
+ * Sync settings
  *
-  * @type {{sync}}
+ * @type {{update, load}}
  */
-var Sync = (function() { // jshint ignore:line
+var Sync = (function () { // jshint ignore:line
 
-    var sync = function() {
+    var readFile = function (url, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", url, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(JSON.parse(rawFile.responseText));
+            }
+        };
 
+        rawFile.send(null);
+    };
+
+    var saveFile = function (url, settings) {
+        //TODO: Implement
+    };
+
+    var processUpdate = function (current, updated) {
+        //TODO: Implement
+    };
+
+    var update = function (settings, url) {
+        var current = readFile(url);
+        var result = processUpdate(current, settings);
+        saveFile(url, result);
+    };
+
+    var load  = function (url, callback) {
+        readFile(url, callback);
     };
 
     // EXPOSE
     return {
-        sync: sync
+        update: update,
+        load: load
     };
 })();
--- Extension/tests/sync-settings/test-sync-settings.js
+++ Extension/tests/sync-settings/test-sync-settings.js
@@ -1,3 +1,46 @@
-QUnit.test("Sync settings test", function(assert) {
-    assert.ok(true);
+QUnit.test("Test settings", function (assert) {
+    var url = 'sync.json';
+    assert.ok(url != null);
+
+    var done = assert.async();
+
+    var callback = function (current) {
+        assert.ok(current != null);
+        assert.ok(current.timestamp != null);
+        assert.ok(current.filters != null);
+        assert.ok(current.filters["enabled-filters"] != null);
+        assert.ok(current.filters["enabled-filters"].length > 0);
+
+        var syncSettingsDTO = {
+            "timestamp": current.timestamp + 100,
+            "filters": {
+                "use-mobile-filters": !current.filters["use-mobile-filters"],
+                "enabled-filters": [
+                    {
+                        "id": 2
+                    },
+                    {
+                        "id": 3
+                    },
+                    {
+                        "url": "http://filter-url-updated"
+                    }
+                ]
+            }
+        };
+
+        //Sync.update(syncSettingsDTO, url);
+        //
+        //var updated = Sync.load(url);
+        //assert.ok(updated != null);
+        //assert.equal(updated.timestamp, syncSettingsDTO.timestamp);
+        //assert.ok(updated.filters != null);
+        //assert.equal(updated.filters["use-mobile-filters"], syncSettingsDTO.filters["use-mobile-filters"]);
+        //assert.ok(updated.filters["enabled-filters"] != null);
+        //assert.equal(updated.filters["enabled-filters"].length, syncSettingsDTO.filters["enabled-filters"].length);
+
+        done();
+    };
+
+    Sync.load(url, callback);
 });
