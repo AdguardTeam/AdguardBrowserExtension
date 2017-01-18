f68f699aa08bb6b613b82da45761da950473115a
mizzick <ds.kolyshev@gmail.com>
2017-01-18 20:49:57 +0530

    Fix #507 safari converter generichide
    (cherry picked from commit b16ac31)

--- Extension/browser/safari/lib/converter.js
+++ Extension/browser/safari/lib/converter.js
@@ -18,7 +18,7 @@
 /**
  * Safari content blocking format rules converter.
  */
-var CONVERTER_VERSION = '1.3.22';
+var CONVERTER_VERSION = '1.3.23';
 // Max number of CSS selectors per rule (look at _compactCssRules function)
 var MAX_SELECTORS_PER_WIDE_RULE = 250;
 var ANY_URL_TEMPLATES = ['||*', '', '*'];
