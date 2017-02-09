3cc92845b9ac91c5f74e1dc3ea228fe76624afba
mizzick <ds.kolyshev@gmail.com>
2017-02-09 20:11:32 +0530

    #315 sync tests

--- Extension/tests/prefs.js
+++
@@ -1,7 +0,0 @@
-adguard.prefs.getBrowser = function () {
-    return null;
-};
-adguard.prefs.getLocalFilterPath = function (filterId) {
-    var url = "filter_" + filterId + ".txt";
-    return '../filters/' + url;
-};
