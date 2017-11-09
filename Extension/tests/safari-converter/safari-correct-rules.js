324b7f5c997e7f803dba8614e9f8f90760b4f745
a.tropnikov <atropnikov@adguard.com>
2017-11-09 15:51:39 +0300

    Merge help

--- Extension/tests/safari-converter/safari-correct-rules.js
+++ Extension/tests/safari-converter/safari-correct-rules.js
@@ -212,6 +212,17 @@ var safariCorrectRules = [
     },
     {
         "trigger": {
+            "url-filter": "^[htpsw]+://([^/]*\\.)?hulu-jsinject-image\\.com",
+            "resource-type": [
+                "image"
+            ]
+        },
+        "action": {
+            "type": "ignore-previous-rules"
+        }
+    },
+    {
+        "trigger": {
             "url-filter": ".*",
             "if-domain": [
                 "*test-document.com"
