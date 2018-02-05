a51d2ae7bb57291a6d72af4a33739e37dfc8f158
seanl-adg <seanl@adguard.com>
2018-02-05 20:02:43 +0900

    Resolve comments: add version check in use InsertCSSAndExecuteScript

--- Extension/lib/utils/browser-utils.js
+++ Extension/lib/utils/browser-utils.js
@@ -319,7 +319,24 @@
          * and tabs.executeScript
          */
         useInsertCSSAndExecuteScript: function () {
-            return this.isChromium() || this.isFirefoxBrowser();
+            return (
+                // Support for tabs.insertCSS and tabs.executeScript on chrome 
+                // requires chrome version above or equal to 39.
+                // https://developers.chrome.com/extensions/tabs
+                (this.isChromeBrowser() || this.isOperaBrowser()) &&
+                adguard.prefs.chromeVersion >= 39 
+            ) || (
+                // Support for tabs.insertCSS and tabs.executeScript on firefox
+                // requires firefox version above or equal to 47.
+                // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/tabs/executeScript
+                // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/tabs/insertCSS
+                // So, basically all webextension will be able to use it.
+                this.isFirefoxBrowser()
+            )
+            
+            
+            
+            this.isChromium() || this.isFirefoxBrowser();
         }
     };
 
