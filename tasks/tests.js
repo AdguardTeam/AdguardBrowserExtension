aa2be1f7974417c6a3354fa81da0b763ade85de2
Maxim Topchu <maximtop@gmail.com>
2018-05-16 11:39:57 +0300

    fix

--- tasks/tests.js
+++ tasks/tests.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp';
 import qunit from 'node-qunit-phantomjs';
 
-// qunit error handler: stops gulp execution on tests failure
+// qunit error handler: to fail gulp execution on tests failure
 const errorHandler = (done) => (code) => {
     if(code === 1) {
         throw new Error('Unit Tests Failure');
