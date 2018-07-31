a6873f704bcde51eaffe0062b1613c8c1e7943f4
Maxim Topchu <maximtop@gmail.com>
2018-07-31 12:15:28 +0300

    update filter downloder to v.1.0.6

--- Extension/lib/libs/filter-downloader.js
+++ Extension/lib/libs/filter-downloader.js
@@ -1,6 +1,6 @@
 /**
  * filters-downloader - Compiles filters source files
- * @version v1.0.5
+ * @version v1.0.6
  * @link http://adguard.com
  */
 /**
@@ -20,7 +20,107 @@
  * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* global URL */
+/**
+ * As it is not possible to use one library in node and browser environments,
+ * we have to implementation of simple file download interface.
+ * The one for node uses axios, the one for browser XMLHttpRequest.
+ *
+ * @type {{getLocalFile, getExternalFile}}
+ */
+const FileDownloadWrapper = (() => {
+    "use strict";
+
+    /**
+     * Executes async request
+     *
+     * @param url Url
+     * @param contentType Content type
+     * @returns {Promise}
+     */
+    const executeRequestAsync = (url, contentType) => {
+
+        return new Promise((resolve, reject) => {
+
+            const onRequestLoad = (response) => {
+                if (response.status !== 200 && response.status !== 0) {
+                    throw new Error("Response status is invalid: " + response.status);
+                }
+
+                const responseText = response.responseText ? response.responseText : response.data;
+
+                if (!responseText) {
+                    throw new Error("Response is empty");
+                }
+
+                const lines = responseText.trim().split(/[\r\n]+/);
+                resolve(lines);
+            };
+
+            const request = new XMLHttpRequest();
+
+            try {
+                request.open('GET', url);
+                request.setRequestHeader('Content-type', contentType);
+                request.setRequestHeader('Pragma', 'no-cache');
+                request.overrideMimeType(contentType);
+                request.mozBackgroundRequest = true;
+                request.onload = function () {
+                    onRequestLoad(request);
+                };
+                request.onerror = reject;
+                request.onabort = reject;
+                request.ontimeout = reject;
+
+                request.send(null);
+            } catch (ex) {
+                reject(ex);
+            }
+        });
+    };
+
+    /**
+     * Downloads filter rules from external url
+     *
+     * @param {string} url Filter file absolute URL or relative path
+     * @returns {Promise} A promise that returns {string} with rules when if resolved and {Error} if rejected.
+     */
+    const getExternalFile = (url) => {
+        return executeRequestAsync(url, 'text/plain');
+    };
+
+    /**
+     * Get filter rules from local path
+     *
+     * @param {string} url local path
+     * @returns {Promise} A promise that returns {string} with rules when if resolved and {Error} if rejected.
+     */
+    const getLocalFile = (url) => {
+        return executeRequestAsync(url, 'text/plain');
+    };
+
+    return {
+        getLocalFile: getLocalFile,
+        getExternalFile: getExternalFile
+    }
+})();
+/**
+ * This file is part of Adguard Browser Extension (https://github.com/AdguardTeam/AdguardBrowserExtension).
+ *
+ * Adguard Browser Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Adguard Browser Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Adguard Browser Extension.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* global URL, require, FileDownloadWrapper */
 /**
  * The utility tool resolves preprocessor directives in filter content.
  *
@@ -35,9 +135,11 @@
  * https://github.com/AdguardTeam/AdguardBrowserExtension/issues/917
  */
 
-const axios = require('axios');
-const path = require('path');
-const fs = require('fs');
+// Override FileDownload object for node environment
+if (typeof FileDownloadWrapper === 'undefined') {
+    //noinspection JSAnnotator
+    FileDownloadWrapper = require('./node/file-download-wrapper');
+}
 
 const FilterDownloader = (() => {
     "use strict";
@@ -361,18 +463,7 @@ const FilterDownloader = (() => {
             url = `${filterUrlOrigin}/${url}`;
         }
 
-        return executeRequestAsync(url, 'text/plain').then((response) => {
-            if (response.status !== 200 && response.status !== 0) {
-                throw new Error("Response status is invalid: " + response.status);
-            }
-
-            const responseText = response.responseText ? response.responseText : response.data;
-
-            if (!responseText) {
-                throw new Error("Response is empty");
-            }
-
-            const lines = responseText.trim().split(/[\r\n]+/);
+        return FileDownloadWrapper.getExternalFile(url, filterUrlOrigin, definedProperties).then((lines) => {
             filterUrlOrigin = getFilterUrlOrigin(url, filterUrlOrigin);
             return resolveIncludes(lines, filterUrlOrigin, definedProperties);
         });
@@ -388,9 +479,9 @@ const FilterDownloader = (() => {
      */
     const getLocalFile = (url, filterUrlOrigin, definedProperties) => {
         filterUrlOrigin = getFilterUrlOrigin(url, filterUrlOrigin);
-        const file = fs.readFileSync(path.resolve(filterUrlOrigin, url)).toString();
-        const lines = file.trim().split(/[\r\n]+/);
-        return resolveIncludes(lines, filterUrlOrigin, definedProperties);
+        return FileDownloadWrapper.getLocalFile(url, filterUrlOrigin, definedProperties).then((lines) => {
+            return resolveIncludes(lines, filterUrlOrigin, definedProperties);
+        });
     };
 
     /**
@@ -451,4 +542,6 @@ const FilterDownloader = (() => {
     };
 })();
 
-module.exports = FilterDownloader;
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = FilterDownloader;
+}
