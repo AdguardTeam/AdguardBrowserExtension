a593232280db8f082ca04f7015a37abc25bfba0e
Maxim Topchu <maximtop@gmail.com>
2018-08-07 17:21:03 +0300

    count mutations immediately when they fire

--- Extension/lib/utils/css-hits-counter.js
+++ Extension/lib/utils/css-hits-counter.js
@@ -101,46 +101,41 @@ var CssHitsCounter = (function () { // jshint ignore:line
         },
     };
 
-    /**
-     * Main calculation function.
-     * 1. Select sub collection from elements.
-     * 2. For each element from sub collection: retrieve calculated css 'content' attribute and if it contains 'adguard' marker then retrieve rule text and filter identifier.
-     * 3. Start next task with some delay.
-     *
-     * @param elements Collection of all elements
-     * @param start Start of batch
-     * @param end End of batch
-     * @param step Size of batch
-     * @param result Collection for save result
-     * @param callback Finish callback
-     */
-    function countCssHitsBatch(elements, start, end, step, result, callback) {
-        var length = Math.min(end, elements.length);
-        for (var i = start; i < length; i++) {
+    function getCssHitData(element) {
+        var style = getComputedStyle(element);
+        if (!style) {
+            return {};
+        }
+        var content = style.content;
+        if (!content || content.indexOf(CONTENT_ATTR_PREFIX) < 0) {
+            return {};
+        }
+        var filterIdAndRuleText = decodeURIComponent(content);
+        // 'content' value may include open and close quotes.
+        filterIdAndRuleText = removeQuotes(filterIdAndRuleText);
+        // Remove prefix
+        filterIdAndRuleText = filterIdAndRuleText.substring(CONTENT_ATTR_PREFIX.length);
+        // Attribute 'content' in css looks like: {content: 'adguard{filterId};{ruleText}'}
+        var index = filterIdAndRuleText.indexOf(';');
+        if (index < 0) {
+            return {};
+        }
+        var filterId = parseInt(filterIdAndRuleText.substring(0, index), 10);
+        var ruleText = filterIdAndRuleText.substring(index + 1);
+        return { filterId, ruleText };
+    }
 
+    function countCssHitsForElements(elements, start, length) {
+        const RULE_FILTER_SEPARATOR = ';';
+        start = start || 0;
+        length = length || elements.length;
+        let result = [];
+        for (var i = 0; i < length; i += 1) {
             var element = elements[i];
-            var style = getComputedStyle(element);
-            if (!style) {
+            const { filterId, ruleText } = getCssHitData(element);
+            if (typeof filterId === 'undefined' || typeof ruleText === 'undefined') {
                 continue;
             }
-            var content = style.content;
-            if (!content || content.indexOf(CONTENT_ATTR_PREFIX) < 0) {
-                continue;
-            }
-
-            var filterIdAndRuleText = decodeURIComponent(content);
-            // 'content' value may include open and close quotes.
-            filterIdAndRuleText = removeQuotes(filterIdAndRuleText);
-            // Remove prefix
-            filterIdAndRuleText = filterIdAndRuleText.substring(CONTENT_ATTR_PREFIX.length);
-            // Attribute 'content' in css looks like: {content: 'adguard{filterId};{ruleText}'}
-            var index = filterIdAndRuleText.indexOf(';');
-            if (index < 0) {
-                continue;
-            }
-            var filterId = parseInt(filterIdAndRuleText.substring(0, index), 10);
-            var ruleText = filterIdAndRuleText.substring(index + 1);
-            var RULE_FILTER_SEPARATOR = ';';
             var ruleAndFilterString = filterId + RULE_FILTER_SEPARATOR + ruleText;
             if (HitsCounterStorage.isCounted(element, ruleAndFilterString)) {
                 continue;
@@ -152,45 +147,45 @@ var CssHitsCounter = (function () { // jshint ignore:line
                 element: elementToString(element),
             });
         }
+        return result;
+    }
 
+    function countCssHitsImmediately(elements, callback) {
+        const result = countCssHitsForElements(elements);
+        callback(result);
+    }
+
+    /**
+     * Main calculation function.
+     * 1. Select sub collection from elements.
+     * 2. For each element from sub collection: retrieve calculated css 'content' attribute and if it contains 'adguard'
+     * marker then retrieve rule text and filter identifier.
+     * 3. Start next task with some delay.
+     *
+     * @param elements Collection of all elements
+     * @param start Start of batch
+     * @param end End of batch
+     * @param step Size of batch
+     * @param result Collection for save result
+     * @param callback Finish callback
+     */
+    function countCssHitsBatch(elements, start, end, step, result, callback) {
+        const COUNT_CSS_HITS_BATCH_DELAY = 50;
+        var length = Math.min(end, elements.length);
+        result = result.concat(countCssHitsForElements(elements, start, length));
         if (length === elements.length) {
             callback(result);
             return;
         }
-
         start = end;
         end += step;
-
         // Start next task with some delay
         setTimeout(function () {
             countCssHitsBatch(elements, start, end, step, result, callback);
-        }, 50);
+        }, COUNT_CSS_HITS_BATCH_DELAY);
     }
 
-    function countCssHitsForMutations() {
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-        if (!MutationObserver) {
-            return;
-        }
-        var observer = new MutationObserver(function (mutationRecords) {
-            var potentialElementsWithNewHits = [];
-            mutationRecords.forEach(function (mutationRecord) {
-                var mutationTarget = mutationRecord.target;
-                potentialElementsWithNewHits.push(mutationTarget);
-                var mutationTargetElements = mutationTarget.querySelectorAll('*');
-                for (var i = 0; i < mutationTargetElements.length; i += 1) {
-                    var element = mutationTargetElements[i];
-                    if (potentialElementsWithNewHits.indexOf(element) === -1) {
-                        potentialElementsWithNewHits.push(element);
-                    }
-                }
-            });
-            countCssHitsBatch(potentialElementsWithNewHits, 0, 100, 100, [], function (result) {
-                if (result.length > 0 && typeof onCssHitsFoundCallback === 'function') {
-                    onCssHitsFoundCallback(result);
-                }
-            });
-        });
+    function startObserver(observer) {
         observer.observe(document.documentElement, {
             childList: true,
             subtree: true,
@@ -198,32 +193,76 @@ var CssHitsCounter = (function () { // jshint ignore:line
         });
     }
 
-    /**
-     * This function divides calculation process into tasks.
-     * When calculation finishes, sends results to background page.
-     * See countCssHitsBatch for details.
-     */
-    function countCssHits() {
+    function removeElements(elements) {
+        for (let i = 0; i < elements.length; i += 1) {
+            const element = elements[i];
+            element.remove();
+        }
+    }
+
+    function countAllCssHits() {
         var elements = document.querySelectorAll('*');
-        // Submit first task.
         countCssHitsBatch(elements, 0, 100, 100, [], function (result) {
             if (result.length > 0 && typeof onCssHitsFoundCallback === 'function') {
                 onCssHitsFoundCallback(result);
             }
         });
-        countCssHitsForMutations();
     }
 
-    /**
-     * Invoke countCssHits with delay.
-     * Delay is 2 seconds
-     */
-    function countCssHitsLater() {
+    function countCssHitsForMutations() {
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        if (!MutationObserver) {
+            return;
+        }
+        var COUNT_ALL_CSS_HITS_TIMEOUT = 500;
+        var timeoutId;
+        var observer = new MutationObserver(function (mutationRecords) {
+            // TODO: Collect probe elements, count them, then remove from their targets
+            var probeElements = [];
+            mutationRecords.forEach(function (mutationRecord) {
+                if (mutationRecord.addedNodes.length > 0) {
+                    for (let i = 0; i < mutationRecord.addedNodes.length; i += 1) {
+                        const node = mutationRecord.addedNodes[i];
+                        const { target } = mutationRecord;
+                        if (!node.parentNode && target && target instanceof Node) {
+                            // Most likely this is a "probe" element that was added and then immediately removed from DOM.
+                            // We re-add it and check if any rule matched it
+                            probeElements.push(node);
+                            observer.disconnect();
+                            mutationRecord.target.appendChild(node);
+                            startObserver(observer);
+                        }
+                    }
+                }
+            });
 
-        // Cleanup event listener
-        removeEventListener('load', countCssHitsLater);
+            if (probeElements.length > 0) {
+                countCssHitsImmediately(probeElements, function (result) {
+                    if (result.length > 0 && typeof onCssHitsFoundCallback === 'function') {
+                        onCssHitsFoundCallback(result);
+                        // remove probeElements after sending result
+                        observer.disconnect();
+                        removeElements(probeElements);
+                        startObserver(observer);
+                    }
+                });
+            }
 
-        setTimeout(countCssHits, 2000);
+            // debounce counting all css hits when mutation record fires
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+            timeoutId = setTimeout(function () {
+                countAllCssHits();
+                clearTimeout(timeoutId);
+            }, COUNT_ALL_CSS_HITS_TIMEOUT);
+        });
+        startObserver(observer);
+    }
+
+    function countCssHits() {
+        countAllCssHits();
+        countCssHitsForMutations();
     }
 
     /**
@@ -232,10 +271,11 @@ var CssHitsCounter = (function () { // jshint ignore:line
      */
     var count = function () {
         // 'load' has already fired
-        if (document.readyState === 'complete') {
-            countCssHitsLater();
+        if (document.readyState === 'complete' ||
+            document.readyState === 'interactive') {
+            countCssHits();
         } else {
-            addEventListener('load', countCssHitsLater);
+            addEventListener('DOMContentLoaded', countCssHits);
         }
     };
 
@@ -252,7 +292,6 @@ var CssHitsCounter = (function () { // jshint ignore:line
 
     return {
         count: count,
-        setCssHitsFoundCallback: setCssHitsFoundCallback
+        setCssHitsFoundCallback: setCssHitsFoundCallback,
     };
-
 })();
