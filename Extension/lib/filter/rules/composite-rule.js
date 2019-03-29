95630497defd16279b4d4a9b69285bc194998a28
Tim Shenin <t.shenin@adguard.com>
2019-03-29 17:44:18 +0300

    Add desc to composite rule

--- Extension/lib/filter/rules/composite-rule.js
+++ Extension/lib/filter/rules/composite-rule.js
@@ -18,8 +18,19 @@
 (function (api) {
 
     /**
-     * Composite Rule
+     * This rule may contain a list of rules generated from one complex ruleText
      * @constructor
+     * 
+     * @example
+     * input
+     * ABP snippet rule
+     * `example.org#$#hide-if-has-and-matches-style someSelector; hide-if-contains someSelector2`
+     * 
+     * output
+     * Adguard scriptlet rules
+     * `example.org#%#//scriptlet("hide-if-has-and-matches-style", "someSelector")`
+     * `example.org#%#//scriptlet("hide-if-contains", "someSelector2")`
+     * 
      */
     function CompositeRule(ruleText, rules) {
         this.ruleText = ruleText;
