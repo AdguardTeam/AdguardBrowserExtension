4871e8f072cac992b7c5d98d0188e32ff91c3e9f
Maxim Topchu <maximtop@gmail.com>
2019-04-24 13:43:28 +0300

    fix/1380 move style pseudo class convertion from css-filter-rule to converter

--- Extension/lib/filter/rule-converter.js
+++ Extension/lib/filter/rule-converter.js
@@ -38,7 +38,7 @@
 
     /**
      * Return array of strings separated by space which not in quotes
-     * @param {string} str 
+     * @param {string} str
      */
     function getSentences(str) {
         const reg = /'.*?'|".*?"|\S+/g;
@@ -47,7 +47,7 @@
 
     /**
      * Returns substring enclosed in the widest braces
-     * @param {string} str 
+     * @param {string} str
      */
     function getStringInBraces(str) {
         const firstIndex = str.indexOf('(');
@@ -57,7 +57,7 @@
 
     /**
      * Wrap str in double qoutes and replaces single quotes if need
-     * @param {string} str 
+     * @param {string} str
      */
     function wrapInDoubleQuotes(str) {
         if (str[0] === '\'' && str[str.length - 1] === '\'') {
@@ -67,13 +67,13 @@
             str = str.substring(1, str.length - 1);
             str = str.replace(/\'/g, '\\\'');
         }
-        return `"${str}"`
+        return `"${str}"`;
     }
 
 
     /**
      * Replace string with data by placeholders
-     * @param {string} str 
+     * @param {string} str
      * @param {Object} data where keys is placeholdes names
      */
     function replacePlaceholders(str, data) {
@@ -88,11 +88,11 @@
      * Convert string of UBO scriptlet rule to AdGuard scritlet rule
      * @param {string} rule UBO scriptlet rule
      */
-    function convertUBOScriptletRule(rule) {
+    function convertUboScriptletRule(rule) {
         const domains = stringUtils.getBeforeRegExp(rule, UBO_SCRIPTLET_MASK_REG);
         const args = getStringInBraces(rule)
             .split(/, /g)
-            .map((arg, index) => index === 0 ? `ubo-${arg}` : arg)
+            .map((arg, index) => (index === 0 ? `ubo-${arg}` : arg))
             .map(arg => wrapInDoubleQuotes(arg))
             .join(', ');
 
@@ -106,54 +106,110 @@
      * Convert string of ABP scriptlet rule to AdGuard scritlet rule
      * @param {string} rule UBO scriptlet rule
      */
-    function convertABPSnippetRule(rule) {
+    function convertAbpSnippetRule(rule) {
         const SEMICOLON_DIVIDER = /;(?=(?:(?:[^"]*"){2})*[^"]*$)/g;
         const domains = stringUtils.substringBefore(rule, ABP_SCRIPTLET_MASK);
-        let args = stringUtils.substringAfter(rule, ABP_SCRIPTLET_MASK);
+        const args = stringUtils.substringAfter(rule, ABP_SCRIPTLET_MASK);
         return args.split(SEMICOLON_DIVIDER)
             .map(args => getSentences(args)
                 .filter(arg => arg)
-                .map((arg, index) => index === 0 ? `abp-${arg}` : arg)
+                .map((arg, index) => (index === 0 ? `abp-${arg}` : arg))
                 .map(arg => wrapInDoubleQuotes(arg))
-                .join(', ')
-            )
-            .map(args => replacePlaceholders(ADGUARD_SCRIPTLET_MASK, { domains, args }))
+                .join(', '))
+            .map(args => replacePlaceholders(ADGUARD_SCRIPTLET_MASK, { domains, args }));
     }
 
     /**
      * Check is uBO scriptlet rule
      * @param {string} rule rule text
      */
-    function isUBOScriptletRule(rule) {
+    function isUboScriptletRule(rule) {
         return (
             rule.indexOf(UBO_SCRIPTLET_MASK_1) > -1
             || rule.indexOf(UBO_SCRIPTLET_MASK_2) > -1
         )
         && UBO_SCRIPTLET_MASK_REG.test(rule);
-    };
+    }
 
     /**
      * Check is AdBlock Plus snippet
      * @param {string} rule rule text
      */
-    function isABPSnippetRule(rule) {
+    function isAbpSnippetRule(rule) {
         return rule.indexOf(ABP_SCRIPTLET_MASK) > -1 && rule.search(ADG_CSS_MASK_REG) === -1;
-    };
+    }
+
+    /**
+     * Returns false or converted rule
+     *
+     * Example:
+     * "example.com##h1:style(background-color: blue !important)"
+     * -> "example.com##h1 {background-color: blue !important}"
+     *
+     * @param {string} ruleText - rule text to check if should be checked and if necessary converted
+     * @return {string|boolean} - converted rule text or false
+     */
+    function convertUboCssStyleRule(ruleText) {
+        const UBO_CSS_RULE_MARKERS = {
+            MASK_CSS_RULE: '##',
+            MASK_CSS_EXCEPTION_RULE: '#@#',
+            MASK_CSS_EXTENDED_CSS_RULE: '#?#',
+            MASK_CSS_EXCEPTION_EXTENDED_CSS_RULE: '#@?#',
+        };
+
+        const CSS_TO_INJECT_PAIRS = {
+            [UBO_CSS_RULE_MARKERS.MASK_CSS_RULE]: '#$#',
+            [UBO_CSS_RULE_MARKERS.MASK_CSS_EXCEPTION_RULE]: '#@$#',
+            [UBO_CSS_RULE_MARKERS.MASK_CSS_EXTENDED_CSS_RULE]: '#$?#',
+            [UBO_CSS_RULE_MARKERS.MASK_CSS_EXCEPTION_EXTENDED_CSS_RULE]: '#@$?#',
+        };
+
+        const RULE_MARKER_FIRST_CHAR = '#';
+
+        const UBO_CSS_STYLE_PSEUDO_CLASS = ':style(';
+
+        const mask = api.FilterRule.findRuleMarker(
+            ruleText,
+            Object.values(UBO_CSS_RULE_MARKERS),
+            RULE_MARKER_FIRST_CHAR
+        );
+        if (!mask) {
+            return false;
+        }
+        const maskIndex = ruleText.indexOf(mask);
+        const cssContent = ruleText.substring(maskIndex + mask.length);
+        const shouldConvert = cssContent.indexOf(UBO_CSS_STYLE_PSEUDO_CLASS) > -1;
+        if (!shouldConvert) {
+            return false;
+        }
+
+        const domainsPart = ruleText.substring(0, maskIndex);
+        const regex = /:style\s*\(\s*(\S+.*\S)\s*\)/;
+        const subst = ' { $1 }';
+        const convertedCssContent = cssContent.replace(regex, subst);
+        if (convertedCssContent === cssContent) {
+            throw new Error(`Empty :style pseudo class: ${cssContent}`);
+        }
+        return domainsPart + CSS_TO_INJECT_PAIRS[mask] + convertedCssContent;
+    }
 
     /**
      * Convert external scriptlet rule to AdGuard scriptlet syntax
      * @param {string} rule convert rule
      */
     function convertRule(rule) {
-        if (isUBOScriptletRule(rule)) {
-            return convertUBOScriptletRule(rule);
+        if (isUboScriptletRule(rule)) {
+            return convertUboScriptletRule(rule);
         }
-        if (isABPSnippetRule(rule)) {
-            return convertABPSnippetRule(rule);
+        if (isAbpSnippetRule(rule)) {
+            return convertAbpSnippetRule(rule);
+        }
+        const uboCssStyleRule = convertUboCssStyleRule(rule);
+        if (uboCssStyleRule) {
+            return uboCssStyleRule;
         }
         return rule;
     }
 
     api.ruleConverter = { convertRule };
-
 })(adguard, adguard.rules);
