72c946ebe961d404e8280bfe8869c57d4d6d3b31
Maxim Topchu <maximtop@gmail.com>
2019-05-21 11:34:16 +0300

    feature/1378 edit uninstall page url

--- Extension/lib/startup.js
+++ Extension/lib/startup.js
@@ -30,7 +30,7 @@ adguard.initialize = function () {
         });
 
         // Set uninstall page url
-        const uninstallUrl = 'https://dev-welcome.adguard.com/uninstall-ext.html';
+        const uninstallUrl = 'https://welcome.adguard.com/forward.html?action=adguard_uninstal_ext&from=background&app=browser_extension';
         adguard.runtime.setUninstallURL(uninstallUrl, () => {
             if (adguard.runtime.lastError) {
                 adguard.console.error(adguard.runtime.lastError);
