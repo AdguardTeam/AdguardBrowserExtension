58760afe77d312278ced9b75743b14646d4baee3
Maxim Topchu <maximtop@gmail.com>
2019-07-04 20:12:32 +0300

    set opera min version 42

--- tasks/browser-opera.js
+++ tasks/browser-opera.js
@@ -8,9 +8,10 @@
 import fs from 'fs';
 import path from 'path';
 import gulp from 'gulp';
-import {BUILD_DIR, BRANCH_RELEASE, PRIVATE_FILES} from './consts';
-import {version} from './parse-package';
+import modifyFile from 'gulp-modify-file';
 import crx from 'gulp-crx-pack';
+import { BUILD_DIR, BRANCH_RELEASE, PRIVATE_FILES } from './consts';
+import { version } from './parse-package';
 
 // set current type of build
 const BRANCH = process.env.NODE_ENV || '';
@@ -18,14 +19,15 @@ const BRANCH = process.env.NODE_ENV || '';
 const paths = {
     filtersOpera: path.join('Extension/filters/opera/**/*'),
     chromium: path.join(BUILD_DIR, BRANCH, `chrome-${version}`, '**/*'),
+    chromiumManifest: path.join(BUILD_DIR, BRANCH, `chrome-${version}`, 'manifest.json'),
     cert: path.join(PRIVATE_FILES, 'certificate.pem'),
-    dest: path.join(BUILD_DIR, BRANCH, `opera-${version}`)
+    dest: path.join(BUILD_DIR, BRANCH, `opera-${version}`),
 };
 
 const dest = {
     filters: path.join(paths.dest, 'filters'),
     inner: path.join(paths.dest, '**/*'),
-    buildDir: path.join(BUILD_DIR, BRANCH)
+    buildDir: path.join(BUILD_DIR, BRANCH),
 };
 
 // copy chromium build dir
@@ -33,6 +35,19 @@ const copyChromiumFiles = () => gulp.src(paths.chromium).pipe(gulp.dest(paths.de
 
 // replace chromium filters by opera filters
 const copyFiltersOpera = () => gulp.src(paths.filtersOpera).pipe(gulp.dest(dest.filters));
+
+// copy chromium manifest, and update it
+const modifyManifest = () => gulp
+    .src(paths.chromiumManifest)
+    .pipe(modifyFile((content, path, file) => {
+        const manifest = JSON.parse(content);
+        const updatedManifest = {
+            ...manifest,
+            minimum_opera_version: '42',
+        };
+        return JSON.stringify(updatedManifest, null, 4);
+    }))
+    .pipe(gulp.dest(paths.dest));
 
 const crxPack = (done) => {
     if (BRANCH !== BRANCH_RELEASE) {
@@ -42,9 +57,9 @@ const crxPack = (done) => {
     return gulp.src(paths.dest)
         .pipe(crx({
             privateKey: fs.readFileSync(paths.cert, 'utf8'),
-            filename: `opera-${BRANCH}-${version}.crx`
+            filename: `opera-${BRANCH}-${version}.crx`,
         }))
         .pipe(gulp.dest(dest.buildDir));
 };
 
-export default gulp.series(copyChromiumFiles, copyFiltersOpera, crxPack);
+export default gulp.series(copyChromiumFiles, copyFiltersOpera, modifyManifest, crxPack);
