83554c7e2f55e23172e658ca6b93e91ed0f4d427
Maxim Topciu <maximtop@gmail.com>
2020-08-25 19:22:50 +0300

    AG-2565 remove gulp from download-filters

--- tools/constants.js
+++ tools/constants.js
@@ -1,3 +1,5 @@
+import { EXTENSION_FILTERS_SERVER_URL_FORMAT } from '../tasks/consts';
+
 export const ENVS = {
     DEV: 'dev',
     BETA: 'beta',
@@ -18,3 +20,11 @@ export const BROWSERS = {
     CHROME: 'chrome',
     FIREFOX: 'firefox',
 };
+
+// filters constants
+export const METADATA_DOWNLOAD_URL_FORMAT = `${EXTENSION_FILTERS_SERVER_URL_FORMAT}/filters.json`;
+export const FILTERS_DEST = 'Extension/filters/%browser';
+export const METADATA_I18N_DOWNLOAD_URL_FORMAT = `${EXTENSION_FILTERS_SERVER_URL_FORMAT}/filters_i18n.js`;
+export const LAST_ADGUARD_FILTER_ID = 14;
+export const FILTER_DOWNLOAD_URL_FORMAT = `${EXTENSION_FILTERS_SERVER_URL_FORMAT}/filters/%filter.txt`;
+export const OPTIMIZED_FILTER_DOWNLOAD_URL_FORMAT = `${EXTENSION_FILTERS_SERVER_URL_FORMAT}/filters/%s_optimized.txt`;
--- tools/resources.js
+++ tools/resources.js
@@ -1,1 +1,13 @@
 // TODO update filters, update suffixes
+import { downloadFilters } from './resources/download-filters';
+
+const resources = async () => {
+    try {
+        await downloadFilters();
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+    }
+};
+
+resources();
