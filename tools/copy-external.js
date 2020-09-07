38239823af65b2df8a280e655141f0f2ea03e267
Maxim Topciu <maximtop@gmail.com>
2020-09-07 14:20:37 +0300

    AG-2565 copy redirects only

--- tools/copy-external.js
+++ tools/copy-external.js
@@ -1,27 +1,17 @@
 import cpy from 'cpy';
 
-// TODO move to modules, import assistant into content script, and scriptlets into background page
 export const copyExternals = async () => {
     const paths = [
-        // TODO check is scriptlets should be copied as well
-        // redirects
         {
             from: [
+                // TODO check necessity of this file
                 'node_modules/scriptlets/dist/redirects.yml',
-                'node_modules/scriptlets/dist/redirects.js',
             ],
             to: 'Extension/lib/libs/scriptlets',
         },
         {
             from: 'node_modules/scriptlets/dist/redirect-files/*',
             to: 'Extension/web-accessible-resources/redirects',
-        },
-        {
-            to: 'Extension/lib/content-script/assistant',
-            from: 'node_modules/adguard-assistant/dist/assistant.embedded.js',
-            options: {
-                rename: 'assistant.js',
-            },
         },
     ];
     // eslint-disable-next-line no-restricted-syntax
