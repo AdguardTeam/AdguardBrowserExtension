24900a6c23c358d5c75b4e1bc695f3bba716bf2d
Maxim Topciu <maximtop@gmail.com>
2020-09-08 15:41:39 +0300

    AG-2565 remove redundant comments

--- Extension/pages/export/index.js
+++ Extension/pages/export/index.js
@@ -1,8 +1,3 @@
-// <script type="text/javascript" src="../lib/content-script/adguard-content.js"></script>
-// <script type="text/javascript" src="../lib/content-script/common-script.js"></script>
-// <script type="text/javascript" src="../lib/content-script/content-script.js"></script>
-// <script type="text/javascript" src="../lib/pages/export.js"></script>
-
 import { exportPage } from '../../lib/pages/export';
 
 exportPage.init();
