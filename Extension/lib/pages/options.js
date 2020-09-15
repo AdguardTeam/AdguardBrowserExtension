054ca00e87f333098bb0aacae48a4d1704dee0a5
Maxim Topciu <maximtop@gmail.com>
2020-09-15 10:40:20 +0300

    AG-2565 display update info on options page

--- Extension/lib/pages/options.js
+++ Extension/lib/pages/options.js
@@ -2215,4 +2215,4 @@ const init = () => {
 
 export const options = {
     init,
-}
+};
--- Extension/pages/options/index.js
+++ Extension/pages/options/index.js
@@ -1,5 +1,7 @@
 import { options } from '../../lib/pages/options';
+import { contentUtils } from '../../lib/content-script/content-utils';
 
 options.init();
+contentUtils.init();
 
 // TODO import into options pages to display info about updates ../lib/content-script/content-utils.js
