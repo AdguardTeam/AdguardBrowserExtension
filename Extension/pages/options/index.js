d8d63b7f1fba59a13d9a18f012fc65b1e9d2c01f
Maxim Topciu <maximtop@gmail.com>
2020-09-15 10:41:01 +0300

    AG-2565 remove todo

--- Extension/pages/options/index.js
+++ Extension/pages/options/index.js
@@ -3,5 +3,3 @@ import { contentUtils } from '../../lib/content-script/content-utils';
 
 options.init();
 contentUtils.init();
-
-// TODO import into options pages to display info about updates ../lib/content-script/content-utils.js
