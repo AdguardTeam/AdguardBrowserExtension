b2309e1dbfd8d6850025b0aa0b41a99a5a96c944
tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
2020-10-01 00:30:25 +0300

    removed redundant properties from adguard

--- Extension/pages/background/index.js
+++ Extension/pages/background/index.js
@@ -3,9 +3,6 @@ import { webrequest } from '../../lib/webrequest';
 import { requestSanitizer } from '../../lib/filter/request-sanitizer';
 import { localeDetect } from '../../lib/filter/services/locale-detect';
 import { contentMessageHandler } from '../../lib/content-message-handler';
-import { localStorage } from '../../lib/storage';
-import { documentFilterService } from '../../lib/filter/services/document-filter';
-import { safebrowsing } from '../../lib/filter/services/safebrowsing';
 import { tabsApi } from '../../lib/tabs/tabs-api';
 
 start();
@@ -15,11 +12,5 @@ localeDetect.init();
 contentMessageHandler.init();
 
 window.adguard = {
-    // exposed for options page
-    localStorage,
-    // exposed for adBlockedPage
-    documentFilterService,
-    // exposed for safebrowsing
-    safebrowsing,
     tabs: tabsApi,
 };
