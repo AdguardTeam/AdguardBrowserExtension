c50fc1bd265c740a08b5f4897d4949df47942360
tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
2020-10-25 03:55:06 +0300

    ScrollToTop functional component

--- Extension/src/pages/options/components/ScrollToTop/ScrollToTop.jsx
+++ Extension/src/pages/options/components/ScrollToTop/ScrollToTop.jsx
@@ -1,21 +1,29 @@
-import { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class ScrollToTop extends Component {
-    componentDidUpdate(prevProps) {
-        const { location } = this.props;
-        const { location: prevLocation } = prevProps;
+const ScrollToTop = ({ children, location }) => {
+    const usePrevious = (value) => {
+        const ref = useRef();
+        useEffect(() => {
+            ref.current = value;
+        });
+        return ref.current;
+    };
+
+    useEffect(() => {
+        const prevLocation = usePrevious(location);
         if (location !== prevLocation) {
             window.scrollTo(0, 0);
         }
-    }
+    });
 
-    render() {
-        const { children } = this.props;
-        return children;
-    }
-}
+    return (
+        <>
+            {children}
+        </>
+    );
+};
 
 ScrollToTop.propTypes = {
     // eslint-disable-next-line react/forbid-prop-types
