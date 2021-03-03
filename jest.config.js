a207c868bc3e284b578879d7c86a9fe9ac2959ca
Maxim Topciu <maximtop@gmail.com>
2021-03-03 17:06:16 +0200

    AG-6831 fix tests run for node <= 10

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     verbose: true,
     setupFiles: ['./tests/__setups__/chrome.js'],
+    'transformIgnorePatterns': [
+        '<rootDir>/node_modules/(?!@adguard/tsurlfilter/dist/es)',
+    ],
 };
