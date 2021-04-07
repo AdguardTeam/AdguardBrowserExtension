e10c7ff29538a4f9f9d6b53d62093f2aa72882c6
Maxim Topciu <maximtop@gmail.com>
2021-04-07 09:51:04 +0300

    AG-7488 fix sample-api message sending

--- Extension/api/sample-extension/sample.js
+++ Extension/api/sample-extension/sample.js
@@ -30,8 +30,9 @@ adguardApi.onRequestBlocked.addListener(onBlocked);
 // Add event listener for rules created by Adguard Assistant
 chrome.runtime.onMessage.addListener((message) => {
     if (message.type === 'assistant-create-rule') {
-        console.log(`Rule ${message.ruleText} was created by Adguard Assistant`);
-        configuration.rules.push(message.ruleText);
+        const { ruleText } = message.data;
+        console.log(`Rule ${ruleText} was created by Adguard Assistant`);
+        configuration.rules.push(ruleText);
         adguardApi.configure(configuration, () => {
             console.log('Finished Adguard API re-configuration');
         });
