4f1337f712bfb0a2b874ba3d7a1dcabee66f1edb
Slava Leleka <v.leleka@adguard.com>
2021-05-05 13:24:06 +0300

    add findChunks test for escaped character

--- tests/src/pages/helpers.test.js
+++ tests/src/pages/helpers.test.js
@@ -8,6 +8,7 @@ describe('helpers', () => {
             expect(findChunks('AdGuard Base filter', 'filter')).toHaveLength(2);
             expect(findChunks('Peter Lowe\'s Blocklist', 'lo')).toHaveLength(5);
             expect(findChunks('Fanboy\'s Anti-Facebook List', 'Bo')).toHaveLength(5);
+            expect(findChunks('ChinaList+EasyList', '+')).toHaveLength(3);
         });
         it('matches', () => {
             let chunks = findChunks('AdGuard Base filter', 'base');
@@ -20,6 +21,10 @@ describe('helpers', () => {
 
             chunks = findChunks('Fanboy\'s Anti-Facebook List', 'Bo');
             expected = ['Fan', 'bo', 'y\'s Anti-Face', 'bo', 'ok List'];
+            expect(chunks).toEqual(expected);
+
+            chunks = findChunks('ChinaList+EasyList', '+');
+            expected = ['ChinaList', '+', 'EasyList'];
             expect(chunks).toEqual(expected);
         });
     });
