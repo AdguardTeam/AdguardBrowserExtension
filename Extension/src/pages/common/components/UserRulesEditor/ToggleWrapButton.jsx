e54fc01b21a025c3c654279ca125366a14d0412b
Slava Leleka <v.leleka@adguard.com>
2021-06-24 01:35:32 +0300

    fix tooltip for ToggleWrapButton

--- Extension/src/pages/common/components/UserRulesEditor/ToggleWrapButton.jsx
+++ Extension/src/pages/common/components/UserRulesEditor/ToggleWrapButton.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { observer } from 'mobx-react';
 import classnames from 'classnames';
 
 import { userRulesEditorStore } from './UserRulesEditorStore';
 import { Icon } from '../ui/Icon';
-import { translator } from '../../../../common/translators/translator';
 import { reactTranslator } from '../../../../common/translators/reactTranslator';
+import { Tooltip } from '../ui/Tooltip';
 
 /**
  * This button is extracted in the separate file
@@ -14,22 +14,32 @@ import { reactTranslator } from '../../../../common/translators/reactTranslator'
 export const ToggleWrapButton = observer(({ onClick }) => {
     const store = useContext(userRulesEditorStore);
 
+    const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
     const lineBreakClassNames = classnames('actions__btn actions__btn--icon', {
         'actions__btn--active': store.userRulesEditorWrapState,
     });
+
+    const tooltipText = store.userRulesEditorWrapState
+        ? reactTranslator.getMessage('options_userfilter_line_break_on')
+        : reactTranslator.getMessage('options_userfilter_line_break_off');
+
+    const handleMouseMove = () => {
+        setTooltipPosition({ x: -40, y: 30 });
+    };
 
     return (
         <button
             type="button"
             className={lineBreakClassNames}
             onClick={onClick}
+            onMouseMove={handleMouseMove}
         >
             <Icon classname="icon--extend" id="#line-break" />
-            <div className="tooltip">
-                {store.userRulesEditorWrapState
-                    ? reactTranslator.getMessage('options_userfilter_line_break_on')
-                    : reactTranslator.getMessage('options_userfilter_line_break_off')}
-            </div>
+            <Tooltip
+                text={tooltipText}
+                position={tooltipPosition}
+            />
         </button>
     );
 });
