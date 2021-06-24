47369b7e6ba448b5f7a4080641c697c62c1b2224
Slava Leleka <v.leleka@adguard.com>
2021-06-24 21:16:57 +0300

    ditch main footer

--- Extension/src/pages/constants.js
+++ Extension/src/pages/constants.js
@@ -4,7 +4,7 @@ export const PRIVACY_URL = 'https://adguard.com/forward.html?action=privacy&from
 export const ACKNOWLEDGMENTS_URL = 'https://adguard.com/forward.html?action=acknowledgments&from=options_screen&app=browser_extension';
 export const GITHUB_URL = 'https://adguard.com/forward.html?action=github_options&from=options_screen&app=browser_extension';
 export const WEBSITE_URL = 'https://adguard.com/forward.html?action=adguard_site&from=options_screen_footer&app=browser_extension';
-export const FORUM_URL = 'https://adguard.com/forward.html?action=discuss&from=options_screen&app=browser_extension';
+export const DISCUSS_URL = 'https://adguard.com/forward.html?action=discuss&from=options_screen&app=browser_extension';
 export const COMPARE_URL = 'https://adguard.com/forward.html?action=compare&from=options_screen&app=browser_extension';
 export const CHANGELOG_URL = 'https://adguard.com/forward.html?action=github_version_popup&from=options_screen&app=browser_extension';
 export const DEFAULT_FIRST_PARTY_COOKIES_SELF_DESTRUCT_MIN = 4320;
