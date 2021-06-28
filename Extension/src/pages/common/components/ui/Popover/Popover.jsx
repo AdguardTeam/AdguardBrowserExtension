00e6cda9dde274ecb8e0fa0a580c8583b3f1a8aa
Slava Leleka <v.leleka@adguard.com>
2021-06-28 23:08:32 +0300

    improve Popover component

--- Extension/src/pages/common/components/ui/Popover/Popover.jsx
+++ Extension/src/pages/common/components/ui/Popover/Popover.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-import { Portal } from '../../Portal/Portal';
+import { Portal } from '../../Portal';
 import { Tooltip } from '../Tooltip';
 
 const TOOLTIP_SHOW_DELAY_MS = 500;
 
+const DEFAULT_TOOLTIP_STYLE = { display: 'inline-block' };
+
 /*
     Wrap child container for handle tooltips rendering in overlay on hover
 */
-export const Popover = ({ text, children, ...props }) => {
+export const Popover = ({ text, delay, children, ...props }) => {
     const [showTooltip, setShowTooltip] = useState(false);
     const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
 
@@ -31,7 +33,7 @@ export const Popover = ({ text, children, ...props }) => {
 
         timer.current = setTimeout(() => {
             setShowTooltip(true);
-        }, TOOLTIP_SHOW_DELAY_MS);
+        }, delay || TOOLTIP_SHOW_DELAY_MS);
     };
 
     const handleMouseLeave = () => {
@@ -39,7 +41,12 @@ export const Popover = ({ text, children, ...props }) => {
         setShowTooltip(false);
     };
     return (
-        <div {...props} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div
+            style={DEFAULT_TOOLTIP_STYLE}
+            {...props}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
             {showTooltip && (
                 <Portal id="root-portal">
                     <Tooltip position={tooltipPosition} text={text} />
