bd74265d6c40187c794d8863b64c6cd99d5159d1
Vladimir Zhelvis <v.zhelvis@adguard.com>
2021-07-21 12:47:55 +0300

    restore autogenerated validator module, add comment banner

--- tools/genValidators.js
+++ tools/genValidators.js
@@ -12,13 +12,20 @@ const standaloneCode = require('ajv/dist/standalone').default;
 
 const ajv = new Ajv({ code: { source: true } });
 
+const moduleBanner = `/**
+ * DON'T EDIT THIS FILE
+ * Module was generated automatically by ajv precompiler 
+ * See '/tools/bundle/genValidators.js'
+ */
+`;
+
 const createValidatorFile = (schemaPath, outputPath) => {
     const schema = require(schemaPath);
     const validator = ajv.compile(schema);
     const moduleCode = standaloneCode(ajv, validator);
     fs.writeFileSync(
         outputPath,
-        moduleCode,
+        moduleBanner + moduleCode,
         { flag: 'w' },
     );
 };
