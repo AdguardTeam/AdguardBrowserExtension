14483a5fd9d00ea013513e2cdebcd87934c699ad
tvinzz <o.labudko@adguard.com>
2022-12-13 03:15:11 +0700

    added xmas promo 23

--- Extension/src/background/storages/notification.ts
+++ Extension/src/background/storages/notification.ts
@@ -33,44 +33,37 @@ export type Notification = {
      icons?: Record<string, Record<string, string>>,
  };
 
-export const BACK_TO_SCHOOL_22_ID = 'backToSchool22';
+export const XMAS_PROMO_23_ID = 'xmasPromo23';
 
-export const backToSchool22Notification: Notification = {
-    id: BACK_TO_SCHOOL_22_ID,
+export const xmasPromo23Notification: Notification = {
+    id: XMAS_PROMO_23_ID,
     locales: {
         en: {
-            title: 'Back',
-            desc: 'to school',
-            btn: 'Get 40% off',
+            title: 'AdGuard X-mas promo',
+            btn: 'Upgrade protection',
         },
         ru: {
-            title: 'Снова в',
-            desc: 'школу',
-            btn: '-40% на всё',
+            title: 'Новогодняя акция',
+            btn: 'Улучшите защиту',
         },
         es: {
             title: 'Vuelta al',
-            desc: 'cole',
             btn: 'Obtén un 40% off',
         },
         de: {
             title: 'Zurück zur',
-            desc: 'Schule',
             btn: '40% Rabatt',
         },
         fr: {
             title: 'La rentrée',
-            desc: 'scolaire',
             btn: '40% de remise',
         },
         it: {
             title: 'Ritorno a',
-            desc: 'scuola',
             btn: '40% di sconto',
         },
         ko: {
             title: '백 투 스쿨',
-            desc: '세일',
             btn: '40% 할인',
         },
         zh_cn: {
@@ -83,37 +76,30 @@ export const backToSchool22Notification: Notification = {
         },
         uk: {
             title: 'Знову до',
-            desc: 'школи',
             btn: 'Знижка 40%',
         },
         pt_pt: {
             title: 'De volta à',
-            desc: 'escola',
             btn: 'Obter 40% de desconto',
         },
         pt_br: {
             title: 'De volta à',
-            desc: 'escola',
             btn: 'Obtenha 40% de desconto',
         },
         ar: {
             title: 'العودة',
-            desc: 'إلى المدرسة',
             btn: '%احصل على خصم 40',
         },
         be: {
             title: 'Назад у',
-            desc: 'школу',
             btn: 'Атрымайце скідку 40%',
         },
         id: {
             title: 'Kembali ke',
-            desc: 'sekolah',
             btn: 'Dapatkan diskon 40%',
         },
         pl: {
             title: 'Powrót do',
-            desc: 'szkoły',
             btn: 'Zyskaj 40% zniżki',
         },
         tr: {
@@ -122,23 +108,22 @@ export const backToSchool22Notification: Notification = {
         },
         vi: {
             title: 'Trở lại',
-            desc: 'trường học',
             btn: 'Được GIẢM GIÁ 40%',
         },
     },
     text: '',
-    url: 'https://link.adtidy.org/forward.html?action=back_to_school&app=browser_extension',
-    from: '30 August 2022 12:00:00',
-    to: '4 September 2022 23:59:00',
+    url: 'https://link.adtidy.net/forward.html?action=xmas_promo_23&app=browser_extension',
+    from: '22 December 2022 15:00:00',
+    to: '02 January 2023 23:59:00',
     type: 'animated',
     icons: {
         ICON_GREEN: {
-            '19': browser.runtime.getURL('assets/icons/promo-on-19.png'),
-            '38': browser.runtime.getURL('assets/icons/promo-on-38.png'),
+            '19': browser.runtime.getURL('assets/icons/xmas23-on-19.png'),
+            '38': browser.runtime.getURL('assets/icons/xmas23-on-38.png'),
         },
         ICON_GRAY: {
-            '19': browser.runtime.getURL('assets/icons/promo-off-19.png'),
-            '38': browser.runtime.getURL('assets/icons/promo-off-38.png'),
+            '19': browser.runtime.getURL('assets/icons/xmas23-off-19.png'),
+            '38': browser.runtime.getURL('assets/icons/xmas23-off-38.png'),
         },
     },
 };
@@ -147,5 +132,5 @@ export const backToSchool22Notification: Notification = {
  * In-memory notifications mapping
  */
 export const notificationStorage = new Map<string, Notification>([
-    [BACK_TO_SCHOOL_22_ID, backToSchool22Notification],
+    [XMAS_PROMO_23_ID, xmasPromo23Notification],
 ]);
