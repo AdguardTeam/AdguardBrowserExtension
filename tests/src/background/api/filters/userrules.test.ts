ef837ecfa2b51d8045a36d149fd2a2c67a433913
Vladimir Zhelvis <v.zhelvis@adguard.com>
2022-12-23 18:44:36 +0300

    Pull request #1391: Feature/AG-18534 Enable UserRulesApi tests
    Merge in EXTENSIONS/browser-extension from feature/AG-18534 to master
    
    Squashed commit of the following:
    
    commit 73031a43bc62f36402f83cd2048f6ebea336212a
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Dec 23 17:34:20 2022 +0200
    
        update deps
    
    commit b50d2fba304eea67bdb660ad7ffacd464ff59f12
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Dec 23 00:38:25 2022 +0200
    
        restore comment
    
    commit e65e781b5d32f94b177dfd8e333dcf2304681414
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Dec 23 00:33:23 2022 +0200
    
        enable UserRulesApi tests

--- tests/src/background/api/filters/userrules.test.ts
+++ tests/src/background/api/filters/userrules.test.ts
@@ -3,10 +3,9 @@ import { UserRulesApi } from '../../../../../Extension/src/background/api/filter
 
 describe('userRules', () => {
     it('handles invalid rules', () => {
-        // // AG does not support selectors with combinators
-        // const invalidRule = 'bing.com##^#b_results > .b_a';
-        // const result = UserRulesApi.convertRules([invalidRule]);
-        // expect(result).toEqual([]);
-        expect(true).toBeTruthy(); // TODO: fix this test
+        // AG does not support selectors with combinators
+        const invalidRule = 'bing.com##^#b_results > .b_a';
+        const result = UserRulesApi.convertRules([invalidRule]);
+        expect(result).toEqual([]);
     });
 });
