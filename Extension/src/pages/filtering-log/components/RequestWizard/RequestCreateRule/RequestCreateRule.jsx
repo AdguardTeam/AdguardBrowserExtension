39b07d6078c0f12beec93da7a4dd482b47f42e44
Dmitry Seregin <d.seregin@adguard.com>
2023-02-06 13:53:48 +0300

    AG-19319: fixed add rule from filtering log
    Merge in EXTENSIONS/browser-extension from fixes/AG-19319 to master
    
    Squashed commit of the following:
    
    commit a50a94528486c28542c472894a94bf21cce42e0a
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Sun Feb 5 19:51:05 2023 +0400
    
        revert changes in RequestInfo
    
    commit 83dba20bb30114845c9836bb99d5d8b5d4fc4d39
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Sun Feb 5 18:49:26 2023 +0400
    
        fix
    
    commit 34af3601f09b8f0ca63c688dbe3f7fb49cf99cce
    Merge: ce3db7f28 71b3f62de
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Sun Feb 5 17:10:35 2023 +0400
    
        merge master
    
    commit ce3db7f289351cc631f69ee79a90c6ace08fa053
    Merge: b01b53db2 408ba6f36
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Feb 3 17:02:10 2023 +0400
    
        Merge branch 'master' into fixes/AG-19319
    
    commit b01b53db25b004248edc26600d696a1a84912e58
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Feb 2 18:17:18 2023 +0400
    
        fix
    
    commit c778646c8e041e7412dd6c14cc2b9be5cc2143e9
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Feb 2 18:16:27 2023 +0400
    
        AG-19319: fixed add rule from filtering log

--- Extension/src/pages/filtering-log/components/RequestWizard/RequestCreateRule/RequestCreateRule.jsx
+++ Extension/src/pages/filtering-log/components/RequestWizard/RequestCreateRule/RequestCreateRule.jsx
@@ -157,7 +157,7 @@ const RequestCreateRule = observer(() => {
     };
 
     const handleAddRuleClick = async () => {
-        await messenger.filteringLogAddUserRule(wizardStore.rule);
+        await messenger.addUserRule(wizardStore.rule);
         const addedRuleState = wizardStore.requestModalState === WIZARD_STATES.BLOCK_REQUEST
             ? ADDED_RULE_STATES.BLOCK
             : ADDED_RULE_STATES.UNBLOCK;
