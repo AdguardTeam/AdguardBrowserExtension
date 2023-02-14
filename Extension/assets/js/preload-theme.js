027b12c7f90914f2a0e2911964cf3dbbb7ee2f9d
MkQtS <81752398+MkQtS@users.noreply.github.com>
2023-02-14 11:01:51 +0800

    preload-theme: use switch instead of multiple if statements

--- Extension/assets/js/preload-theme.js
+++ Extension/assets/js/preload-theme.js
@@ -23,18 +23,20 @@
 (function () {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const theme = urlSearchParams.get('theme');
-    /*
-        if theme parameter is missing or a system theme is selected,
-        the desired color is selected using the css media query
-    */
-    if (!theme || theme === 'system') {
-        return;
-    }
-    // the color changes through the selector so that it could be rewritten by css from the main bundle
-    if (theme === 'dark') {
-        document.body.classList.add('body_dark');
-    }
-    if (theme === 'light') {
-        document.body.classList.add('body_light');
+    switch (theme) {
+        // the color changes through the selector so that it could be rewritten by css from the main bundle
+        case 'dark':
+            document.body.classList.add('body_dark');
+            break;
+        case 'light':
+            document.body.classList.add('body_light');
+            break;
+        /*
+            if theme parameter is missing or a system theme is selected,
+            the desired color is selected using the css media query
+        */
+        case 'system':
+        default:
+            break;
     }
 })();
