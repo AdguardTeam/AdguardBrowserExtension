0178c513734193af909ab81eaa77217b686a5b45
Slava Leleka <v.leleka@adguard.com>
2024-04-04 17:31:27 +0300

    enable group on filter enabling from thank-you page. #2776 AG-31704
    Squashed commit of the following:
    
    commit cfaeab230dc9a1209394128418c9ec8fcf856bb2
    Merge: 5280a725c ad07f316d
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Apr 4 15:41:45 2024 +0300
    
        Merge branch 'master' into fix/AG-31704
    
    commit 5280a725cb6f1e8f6b2f5a843629e7eb41c3b729
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Apr 4 15:09:48 2024 +0300
    
        enable group on filter enabling from thank-you page

--- Extension/src/background/services/filters.ts
+++ Extension/src/background/services/filters.ts
@@ -88,7 +88,9 @@ export class FiltersService {
     private static onFilterEnable(message: AddAndEnableFilterMessage): number | undefined {
         const { filterId } = message.data;
 
-        FiltersService.enableFilter(filterId);
+        // second arg 'true' is needed to enable not touched group
+        // https://github.com/AdguardTeam/AdguardBrowserExtension/issues/2776
+        FiltersService.enableFilter(filterId, true);
 
         const group = Categories.getGroupByFilterId(filterId);
 
@@ -256,9 +258,10 @@ export class FiltersService {
      * If filter group has not been touched before, it will be activated.
      *
      * @param filterId Id of filter.
+     * @param shouldEnableGroup Flag for enabling the filter group if it has not been touched before.
      */
-    private static async enableFilter(filterId: number): Promise<void> {
-        await FiltersApi.loadAndEnableFilters([filterId], true);
+    private static async enableFilter(filterId: number, shouldEnableGroup = false): Promise<void> {
+        await FiltersApi.loadAndEnableFilters([filterId], true, shouldEnableGroup);
         Engine.debounceUpdate();
     }
 }
