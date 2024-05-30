c581a487f72f853d391b109eec7df2f22f81318d
Maxim Topciu <mtopciu@adguard.com>
2024-05-30 19:37:26 +0300

    convert footer component to ts

--- Extension/src/pages/options/components/Footer/Footer.tsx
+++ Extension/src/pages/options/components/Footer/Footer.tsx
@@ -19,7 +19,7 @@
 import React, { useContext } from 'react';
 import { observer } from 'mobx-react';
 
-import { reactTranslator } from '../../../../common/translators/reactTranslator';
+import { translator } from '../../../../common/translators/translator';
 import { messenger } from '../../../services/messenger';
 import { Icon } from '../../../common/components/ui/Icon';
 import { rootStore } from '../../stores/RootStore';
@@ -44,20 +44,20 @@ export const Footer = observer(() => {
                 <div className="footer__rate">
                     <div className="footer__in footer__in--rate container">
                         <div className="footer__rate-desc">
-                            {reactTranslator.getMessage('options_do_you_like_question')}
+                            {translator.getMessage('options_do_you_like_question')}
                         </div>
                         <button
                             type="button"
                             className="button button--green button--s"
                             onClick={handleRateClick}
                         >
-                            {reactTranslator.getMessage('options_footer_like_us_cta')}
+                            {translator.getMessage('options_footer_like_us_cta')}
                         </button>
                         <button
                             type="button"
                             className="footer__rate-close"
                             onClick={hideRate}
-                            aria-label={reactTranslator.getMessage('close_button_title')}
+                            aria-label={translator.getMessage('close_button_title')}
                         >
                             <Icon id="#cross" classname="icon--cross" />
                         </button>
