ab6291726911404113d2904ec602edeaa59ad0c5
Dmitriy Seregin <d.seregin@adguard.com>
2024-12-13 15:14:44 +0300

    set up aliases for vitest from tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,10 +15,30 @@
  * You should have received a copy of the GNU General Public License
  * along with AdGuard Browser Extension. If not, see <http://www.gnu.org/licenses/>.
  */
+import path from 'node:path';
+import { readFile } from 'fs/promises';
+
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { MANIFEST_ENV } from './tools/constants';
+
+async function loadJson(pathToFile: string) {
+    const jsonData = await readFile(new URL(pathToFile, import.meta.url));
+    const data = JSON.parse(jsonData.toString());
+
+    return data;
+}
+
+const aliases = (async () => {
+    const TsConfigWithManifestDependantTypes = await loadJson(`./tsconfig.with_types_mv${MANIFEST_ENV}.json`);
+
+    const res = Object.entries(TsConfigWithManifestDependantTypes.compilerOptions.paths).map(([key, value]) => {
+        return [key, path.resolve(__dirname, (value as any)[0])];
+    });
+
+    return Object.fromEntries(res);
+})();
 
 export default defineConfig({
     define: {
@@ -29,11 +49,15 @@ export default defineConfig({
         __IS_MV3__: MANIFEST_ENV === '3',
     },
     plugins: [tsconfigPaths()],
+    resolve: {
+        alias: await aliases,
+    },
     test: {
         setupFiles: './vitest.setup.ts', // Setup file for tests.
         environment: 'jsdom', // Enables jsdom environment for tests.
         environmentOptions: {
             // FIXME: Insert minimal supported browser version here.
+            // eslint-disable-next-line max-len
             userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.0.2468 Yowser/2.5 Safari/537.36',
         },
         reporters: [
