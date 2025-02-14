30fb65b9a5bacf2da0faffeaf808932ddd6ed393
Slava Leleka <v.leleka@adguard.com>
2025-02-14 19:27:23 +0300

    AG-39385 do not report custom filters
    Squashed commit of the following:
    
    commit f18d7848336bc4a856a9a8dcbbf9ca3862d935f7
    Merge: d23b5d470 c83ec4adf
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Fri Feb 14 11:16:48 2025 -0500
    
        Merge branch 'master' into fix/AG-39385-export-and-reporting
    
    commit d23b5d47000fca13abc8e6fee450a72977d51329
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Feb 13 14:53:06 2025 -0500
    
        use runtime.getManifest for manifest_version during issue reporting
    
    commit 15213805b404edb38c07b47e065543b5bb40936b
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Feb 13 16:27:10 2025 -0500
    
        Revert "do not export custom filters"
        
        This reverts commit a0e8b028131dfee92bd02ccdc07216e98a66630c.
    
    commit 88febd7bdb684ce6724c079752dc17396acaf386
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Feb 13 16:21:19 2025 -0500
    
        do not report custom filters
    
    commit a0e8b028131dfee92bd02ccdc07216e98a66630c
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Feb 13 16:20:52 2025 -0500
    
        do not export custom filters

--- Extension/src/background/api/ui/pages.ts
+++ Extension/src/background/api/ui/pages.ts
@@ -31,18 +31,10 @@ import {
     ForwardParams,
 } from '../../../common/forward';
 import { UrlUtils } from '../../utils/url';
-import {
-    browserStorage,
-    groupStateStorage,
-    settingsStorage,
-} from '../../storages';
+import { browserStorage, settingsStorage } from '../../storages';
 import { SettingOption } from '../../schema';
 import { BrowserUtils } from '../../utils/browser-utils';
-import {
-    AntiBannerFiltersId,
-    AntibannerGroupsId,
-    FILTERING_LOG_WINDOW_STATE,
-} from '../../../common/constants';
+import { AntiBannerFiltersId, FILTERING_LOG_WINDOW_STATE } from '../../../common/constants';
 import { WindowsApi, TabsApi } from '../../../common/api/extension';
 import { Prefs } from '../../prefs';
 import { CustomFilterApi, FiltersApi } from '../filters';
@@ -245,12 +237,14 @@ export class PagesApi {
         const commonFilterIds = FiltersApi.getEnabledFilters()
             .filter((filterId) => !CustomFilterApi.isCustomFilter(filterId));
 
+        const manifestDetails = browser.runtime.getManifest();
+
         const params: ForwardParams = {
             action: ForwardAction.IssueReport,
             from,
-            product_type: 'Ext',
-            manifest_version: __IS_MV3__ ? '3' : '2',
-            product_version: encodeURIComponent(browser.runtime.getManifest().version),
+            product_type: 'Extension',
+            manifest_version: encodeURIComponent(manifestDetails.manifest_version),
+            product_version: encodeURIComponent(manifestDetails.version),
             url: encodeURIComponent(siteUrl),
         };
 
@@ -271,16 +265,18 @@ export class PagesApi {
             params.filters = encodeURIComponent(commonFilterIds.join('.'));
         }
 
-        const isCustomFiltersEnabled = groupStateStorage.get(AntibannerGroupsId.CustomFiltersGroupId)?.enabled;
-        if (isCustomFiltersEnabled) {
-            const customFilterUrls = CustomFilterApi.getFiltersData()
-                .filter(({ enabled }) => enabled)
-                .map(({ customUrl }) => UrlUtils.trimFilterFilepath(customUrl));
+        // Ignoring custom filters since AG-39385.
+        // TODO: uncomment when custom filters will be supported again.
+        // const isCustomFiltersEnabled = groupStateStorage.get(AntibannerGroupsId.CustomFiltersGroupId)?.enabled;
+        // if (isCustomFiltersEnabled) {
+        //     const customFilterUrls = CustomFilterApi.getFiltersData()
+        //         .filter(({ enabled }) => enabled)
+        //         .map(({ customUrl }) => UrlUtils.trimFilterFilepath(customUrl));
 
-            if (customFilterUrls.length > 0) {
-                params.custom_filters = encodeURIComponent(customFilterUrls.join(','));
-            }
-        }
+        //     if (customFilterUrls.length > 0) {
+        //         params.custom_filters = encodeURIComponent(customFilterUrls.join(','));
+        //     }
+        // }
 
         Object.assign(
             params,
