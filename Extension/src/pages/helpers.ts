6dc07e8eceb5cddf5a3fb0985da4dc1c5947502b
Slava Leleka <v.leleka@adguard.com>
2025-02-21 16:39:12 -0500

    fix imports

--- Extension/src/pages/helpers.ts
+++ Extension/src/pages/helpers.ts
@@ -16,7 +16,7 @@
  * along with AdGuard Browser Extension. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { type FilterMetadata } from '../background/api';
+import { type FilterMetadata } from '../background/api/filters/main';
 import { translator } from '../common/translators/translator';
 
 import { FILE_WRONG_EXTENSION_CAUSE } from './options/constants';
