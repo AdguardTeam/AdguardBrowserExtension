3525296cc855563d076d3aafbd626bcaa47d6ff3
Dmitriy Seregin <d.seregin@adguard.com>
2025-02-23 12:04:09 +0300

    merge plan from master

--- bamboo-specs/scripts/timeout-wrapper.sh
+++ bamboo-specs/scripts/timeout-wrapper.sh
@@ -3,10 +3,7 @@
 # Usage: ./timeout-wrapper.sh <same arguments as timeout command>
 # Example: ./timeout-wrapper.sh 30s pnpm test
 
-# Save the current errexit state, then disable it.
-# Disabling errexit is necessary because the timeout command will return a non-zero exit code if the command times out
-# and errexit would cause the script to exit immediately. We want to capture the exit code and handle it.
-IS_ERREXIT_SET=$(echo $- | grep e | wc -l)
+# Disable errexit to handle the exit code of the command with timeout.
 set +e
 
 # Run the command with timeout
@@ -15,10 +12,8 @@ timeout "$@"
 # Capture exit code
 EXIT_CODE=$?
 
-# Enable errexit if it was enabled before
-if [ $IS_ERREXIT_SET -eq 1 ]; then
-    set -e
-fi
+# Enable errexit back
+set -e
 
 # Handle other failures
 if [ $EXIT_CODE -ne 0 ]; then
