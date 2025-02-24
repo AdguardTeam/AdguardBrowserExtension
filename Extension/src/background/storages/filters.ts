7c696be0a461c2aded75398509779cf8357721a3
scripthunter7 <d.tota@adguard.com>
2025-02-24 18:21:54 +0100

    revert

--- Extension/src/background/storages/filters.ts
+++ Extension/src/background/storages/filters.ts
@@ -64,7 +64,7 @@ export class FiltersStorage {
         let preprocessed: PreprocessedFilterList;
 
         if (typeof filter === 'string') {
-            preprocessed = FilterListPreprocessor.preprocess(filter, false);
+            preprocessed = FilterListPreprocessor.preprocess(filter);
         } else {
             preprocessed = filter;
         }
