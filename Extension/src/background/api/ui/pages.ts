a230967a83ccafedbe1f5218cd8b1836bb239f8d
Slava Leleka <v.leleka@adguard.com>
2025-04-01 13:27:20 -0400

    AG-41047 add test for issue reporting url

--- Extension/src/background/api/ui/pages.ts
+++ Extension/src/background/api/ui/pages.ts
@@ -53,6 +53,15 @@ import { OptionsPageSections } from '../../../common/nav';
  */
 export class PagesApi {
     /**
+     * Product type.
+     *
+     * It has to be an exact string due to the reports docs.
+     *
+     * @see {@link https://github.com/AdguardTeam/ReportsWebApp#pre-filling-the-app-with-query-parameters}
+     */
+    private static readonly PRODUCT_TYPE = 'Ext';
+
+    /**
      * Settings page url.
      */
     public static readonly settingsUrl = PagesApi.getExtensionPageUrl(OPTIONS_OUTPUT);
@@ -219,13 +228,15 @@ export class PagesApi {
     }
 
     /**
-     * Opens abuse page tab.
+     * Creates an url with settings in query parameters needed for issue reporting.
      * Query parameters are described here: https://github.com/AdguardTeam/ReportsWebApp.
      *
      * @param siteUrl Target site url.
      * @param from UI which user is forwarded from.
+     *
+     * @returns Issue report url.
      */
-    public static async openAbusePage(siteUrl: string, from: ForwardFrom): Promise<void> {
+    public static async getIssueReportUrl(siteUrl: string, from: ForwardFrom): Promise<string> {
         let browserName = UserAgent.browserName;
         let browserDetails: string | undefined;
 
@@ -242,7 +253,7 @@ export class PagesApi {
         const params: ForwardParams = {
             action: ForwardAction.IssueReport,
             from,
-            product_type: 'Ext',
+            product_type: PagesApi.PRODUCT_TYPE,
             manifest_version: encodeURIComponent(manifestDetails.manifest_version),
             product_version: encodeURIComponent(manifestDetails.version),
             url: encodeURIComponent(siteUrl),
@@ -285,6 +296,18 @@ export class PagesApi {
         );
 
         const reportUrl = Forward.get(params);
+
+        return reportUrl;
+    }
+
+    /**
+     * Opens abuse page tab.
+     *
+     * @param siteUrl Target site url.
+     * @param from UI which user is forwarded from.
+     */
+    public static async openAbusePage(siteUrl: string, from: ForwardFrom): Promise<void> {
+        const reportUrl = await PagesApi.getIssueReportUrl(siteUrl, from);
 
         await browser.tabs.create({ url: reportUrl });
     }
