ed569ff9f097af8433f7163394cc20bde7a25075
Alexandr Zaitsev <a.zaitsev@adguard.com>
2025-11-13 04:43:36 +0300

    refactor: swap TelemetryDataCollector implementation between MV2 and MV3

--- Extension/src/background/telemetry/TelemetryDataCollector/TelemetryDataCollector-mv2.ts
+++ Extension/src/background/telemetry/TelemetryDataCollector/TelemetryDataCollector-mv2.ts
@@ -15,7 +15,31 @@
  * You should have received a copy of the GNU General Public License
  * along with AdGuard Browser Extension. If not, see <http://www.gnu.org/licenses/>.
  */
+import { SettingsApi } from 'settings-api';
+
+import { FiltersUpdateTime } from '../../../common/constants';
+import { SettingOption } from '../../schema';
+import { FilterUpdateIntervalSource } from '../enums';
 
 import { TelemetryDataCollectorCommon } from './TelemetryDataCollector-common';
 
-export { TelemetryDataCollectorCommon as TelemetryDataCollector };
+/**
+ * Collects and manages telemetry data including synthetic ID, user agent, and application props.
+ *
+ * This class is responsible for gathering all necessary data for telemetry events.
+ */
+export class TelemetryDataCollector extends TelemetryDataCollectorCommon {
+    /**
+     * Gets filter update interval source.
+     *
+     * @returns SystemDefault if using default interval, Custom otherwise.
+     */
+    protected static getFilterUpdateInterval(): FilterUpdateIntervalSource | null {
+        const updatePeriod: FiltersUpdateTime = SettingsApi.getSetting(SettingOption.FiltersUpdatePeriod);
+        const isDefault = updatePeriod === FiltersUpdateTime.OneHour;
+        // TODO: Why is only two values? Why we can't send number info?
+        return isDefault
+            ? FilterUpdateIntervalSource.SystemDefault
+            : FilterUpdateIntervalSource.Custom;
+    }
+}
