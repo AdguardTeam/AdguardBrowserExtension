fdded277223505a3be0734f20171bdc9e2ba1027
Maxim Topciu <mtopciu@adguard.com>
2025-11-21 11:16:45 +0200

    revert to correct link

--- bamboo-specs/scripts/link-tsurlfilter.sh
+++ bamboo-specs/scripts/link-tsurlfilter.sh
@@ -39,7 +39,7 @@ done
 #   TSURLFILTER_REF="v2.1.0"           # tag
 #   TSURLFILTER_REF=""                 # skip cloning
 
-TSURLFILTER_REF="fix/AG-48289"
+TSURLFILTER_REF="release/browser-extension-v5.3"
 
 # Repository URLs
 # This should be set as a Bamboo project variable: tsurlfilterRepoUrl
