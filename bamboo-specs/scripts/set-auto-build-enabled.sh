b01a2f14a766b43e55d476895e288eddfe257598
Alexandr Zaitsev <a.zaitsev@adguard.com>
2025-12-05 13:08:06 +0300

    Fix auto-build logic to use enable flags with OR condition

--- bamboo-specs/scripts/set-auto-build-enabled.sh
+++ bamboo-specs/scripts/set-auto-build-enabled.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-echo "disableAutoBuildsBeta=${bamboo_disableAutoBuildsBeta}"
-echo "disableAutoBuildsRelease=${bamboo_disableAutoBuildsRelease}"
+echo "enableAutoBuildsBeta=${bamboo_enableAutoBuildsBeta}"
+echo "enableAutoBuildsRelease=${bamboo_enableAutoBuildsRelease}"
 
 RESULT_FILE="auto-build.properties"
 
-if [ "${bamboo_disableAutoBuildsBeta}" = "true" ] && [ "${bamboo_disableAutoBuildsRelease}" = "true" ]; then
+if [ "${bamboo_enableAutoBuildsBeta}" = "true" ] || [ "${bamboo_enableAutoBuildsRelease}" = "true" ]; then
   echo "Both flags are true, disabling autobuild"
-  echo "autoBuildEnabled=false" > "${RESULT_FILE}"
+  echo "autoBuildEnabled=true" > "${RESULT_FILE}"
 else
   echo "One of flags is not true, enabling autobuild"
-  echo "autoBuildEnabled=true" > "${RESULT_FILE}"
+  echo "autoBuildEnabled=false" > "${RESULT_FILE}"
 fi
 
 echo "Written ${RESULT_FILE}:"
