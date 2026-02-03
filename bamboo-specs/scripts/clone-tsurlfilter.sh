bd6159f039e27b15daec433ae4f072d4f37d48d0
Anatolii Khamrskii <a.khmarskii@adguard.com>
2026-02-03 17:16:03 +0300

    AG-46552 Support path-in-domain for cosmetic rules by converting it to `Path` modifier

--- bamboo-specs/scripts/clone-tsurlfilter.sh
+++ bamboo-specs/scripts/clone-tsurlfilter.sh
@@ -25,7 +25,7 @@ exec 2>&1
 #   TSURLFILTER_REF="v2.1.0"           # tag
 #   TSURLFILTER_REF=""                 # skip cloning
 
-TSURLFILTER_REF="release/browser-extension-v5.4"
+TSURLFILTER_REF="feature/AG-46552"
 
 # Repository URL
 # This should be set as a Bamboo project variable: tsurlfilterRepoUrl
