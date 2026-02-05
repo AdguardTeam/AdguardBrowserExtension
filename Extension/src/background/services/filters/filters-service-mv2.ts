56094c4abb0d878ca5ace48f619dae5970cd9fda
Alexandr Zaitsev <a.zaitsev@adguard.com>
2026-02-05 10:35:41 +0000

    AG-49823 Extract common logic from the services
    Squashed commit of the following:
    
    commit da622503dd6be646658ddad26e909515bc3a63f9
    Merge: 78b1c9e10 b3a602cdb
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Thu Feb 5 12:27:40 2026 +0300
    
        Merge branch 'release/v5.4' into enhance/AG-49823
    
    commit 78b1c9e1035e91260097b0e63bccbf4de817296a
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Wed Feb 4 19:14:57 2026 +0300
    
        update docs
    
    commit 338c3d40c6484887415b7768cefdc9e255f89c9e
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Wed Feb 4 16:07:44 2026 +0300
    
        refactoring
    
    commit ba4a00e5e6903cc9d0b562fb76b7d178a6579cf5
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Wed Feb 4 13:25:03 2026 +0300
    
        add docs
    
    commit a71da4b1af04e6c422d9a31b9f06dcaf0803b666
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Tue Feb 3 18:54:39 2026 +0300
    
        revert refactoring
    
    commit 6a81894425ff69a62a4ff190ec97ab72121fdbdb
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Fri Jan 30 17:21:14 2026 +0300
    
        refactoring
    
    commit b8c68c21036b6189be844e7c646fedf6732deb4d
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Fri Jan 30 14:31:22 2026 +0300
    
        mv2 enable group fix
    
    commit 321bcae789f3127e4524d1adc22bd3a81a1183c1
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Thu Jan 29 18:31:38 2026 +0300
    
        fix async behavior difference between MV2 and MV3
    
    commit 189f922f09b43117c04edae6921a936382dc4db9
    Merge: 268d51590 9011ba8cf
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Thu Jan 29 15:26:32 2026 +0300
    
        Merge branch 'release/v5.4' into enhance/AG-49823
    
    commit 268d51590559f3d1cc6b09c1523095ac4e529b93
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Thu Jan 22 19:48:54 2026 +0300
    
        refactoring
    
    commit a9a32f7ee0265d218a356aa87463fbafca52ef52
    Author: Alexandr Zaitsev <a.zaitsev@adguard.com>
    Date:   Thu Jan 22 18:24:41 2026 +0300
    
        AG-49823 Extract common logic from the services

--- Extension/src/background/services/filters/filters-service-mv2.ts
+++ Extension/src/background/services/filters/filters-service-mv2.ts
@@ -18,26 +18,15 @@
  * along with AdGuard Browser Extension. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-    type AddAndEnableFilterMessage,
-    type DisableFilterMessage,
-    type DisableFiltersGroupMessage,
-    type EnableFiltersGroupMessage,
-    type GetIsConsentedFilterMessage,
-    MessageType,
-    type SetConsentedFiltersMessage,
-} from '../../../common/messages';
-import { logger } from '../../../common/logger';
+import { type EnableFiltersGroupMessage, MessageType } from '../../../common/messages';
 import { SettingOption } from '../../schema';
 import { messageHandler } from '../../message-handler';
 import { engine } from '../../engine';
 import {
-    annoyancesConsent,
     Categories,
     type FilterMetadata,
     FiltersApi,
     FilterUpdateApi,
-    PageStatsApi,
     toasts,
 } from '../../api';
 import {
@@ -47,13 +36,14 @@ import {
 } from '../../events';
 import { notifier } from '../../notifier';
 import { NotifierType } from '../../../common/constants';
+import { logger } from '../../../common/logger';
+
+import { FiltersServiceCommon } from './filters-service-common';
 
 /**
  * FiltersService creates handlers for messages that relate to filters.
- *
- * **MV2 version**.
  */
-export class FiltersService {
+export class FiltersService extends FiltersServiceCommon {
     /**
      * Adds a listener for background messages about working with filters:
      * disabling, enabling, adding, removing.
@@ -61,151 +51,11 @@ export class FiltersService {
      * Adds a listener for changing the settings of optimized filters and
      * disabling hit collection.
      */
-    public static async init(): Promise<void> {
-        messageHandler.addListener(MessageType.AddAndEnableFilter, FiltersService.onFilterEnable);
-        messageHandler.addListener(MessageType.DisableFilter, FiltersService.onFilterDisable);
-        messageHandler.addListener(MessageType.EnableFiltersGroup, FiltersService.onGroupEnable);
-        messageHandler.addListener(MessageType.DisableFiltersGroup, FiltersService.onGroupDisable);
+    public static override init(): void {
+        super.init();
         messageHandler.addListener(MessageType.CheckFiltersUpdate, FiltersService.manualCheckFiltersUpdate);
-        messageHandler.addListener(MessageType.ResetBlockedAdsCount, FiltersService.resetBlockedAdsCount);
-        messageHandler.addListener(MessageType.SetConsentedFilters, FiltersService.setConsentedFilters);
-        messageHandler.addListener(MessageType.GetIsConsentedFilter, FiltersService.getIsConsentedFilter);
-
         contextMenuEvents.addListener(ContextMenuAction.UpdateFilters, FiltersService.manualCheckFiltersUpdate);
-
         settingsEvents.addListener(SettingOption.UseOptimizedFilters, FiltersService.onOptimizedFiltersSwitch);
-    }
-
-    /**
-     * Enables filter on {@link AddAndEnableFilterMessage} message via {@link FiltersService.enableFilter}.
-     * If filter group has not been touched before, it will be activated.
-     *
-     * If filter group has been touched before and it is disabled now, the engine will not be updated.
-     *
-     * NOTE: we do not await of async task execution and returns group id optimistic.
-     * TODO (v.zhelvis): handle enabling of group on frontend instead using this handler,
-     * because this is UI edge case.
-     *
-     * @param message Message of {@link AddAndEnableFilterMessage} with filter
-     * id to enable.
-     *
-     * @returns Id of the enabled filter group, if it has not been touched before, otherwise returns undefined.
-     */
-    private static async onFilterEnable(message: AddAndEnableFilterMessage): Promise<number | undefined> {
-        const { filterId } = message.data;
-
-        logger.trace(`[ext.FiltersService.onFilterEnable]: background received message to enable filter: id='${filterId}', name='${FiltersApi.getFilterName(filterId)}'`);
-
-        // FiltersService.enableFilter() method's second arg is 'true'
-        // because it is needed to enable not touched group
-        // https://github.com/AdguardTeam/AdguardBrowserExtension/issues/2776
-        FiltersService.enableFilter(filterId, true);
-
-        const group = Categories.getGroupByFilterId(filterId);
-
-        if (!group) {
-            return;
-        }
-
-        const { groupId } = group;
-
-        const groupState = Categories.getGroupState(groupId);
-
-        if (!groupState) {
-            return;
-        }
-
-        if (!groupState.touched) {
-            return groupId;
-        }
-
-        if (groupState.enabled) {
-            // update the engine only if the group is enabled
-            engine.debounceUpdate();
-        }
-    }
-
-    /**
-     * Called at the request to disable filter.
-     *
-     * If filter group is disabled, the engine will not be updated.
-     *
-     * @param message Message of {@link DisableFilterMessage} with filter
-     * id to disable.
-     */
-    private static async onFilterDisable(message: DisableFilterMessage): Promise<void> {
-        const { filterId } = message.data;
-
-        logger.trace(`[ext.FiltersService.onFilterDisable]: background received message to disable filter: id='${filterId}', name='${FiltersApi.getFilterName(filterId)}'`);
-
-        FiltersApi.disableFilters([filterId]);
-
-        const group = Categories.getGroupByFilterId(filterId);
-
-        if (!group) {
-            return;
-        }
-
-        const groupState = Categories.getGroupState(group.groupId);
-
-        if (groupState && groupState.enabled) {
-            // update the engine only if the group is enabled
-            engine.debounceUpdate();
-        }
-    }
-
-    /**
-     * Enables group on {@link EnableFiltersGroupMessage} message via {@link FiltersService.enableGroup}.
-     *
-     * If group is activated first time, provides list of recommended filters.
-     * NOTE: we do not await of async task execution and returns array of recommended filters optimistic.
-     * TODO (v.zhelvis): handle enabling of recommended filters on frontend instead using this handler,
-     * because this is UI edge case.
-     *
-     * @param message {@link EnableFiltersGroupMessage} With specified group id.
-     *
-     * @returns Array of recommended filters on first group activation.
-     */
-    private static async onGroupEnable(message: EnableFiltersGroupMessage): Promise<number[] | undefined> {
-        const { groupId } = message.data;
-
-        const group = Categories.getGroupState(groupId);
-
-        logger.trace(`[ext.FiltersService.onGroupEnable]: background received message to enable group: id='${groupId}', name='${Categories.getGroupName(groupId)}'`);
-
-        if (!group) {
-            logger.error(`[ext.FiltersService.onGroupEnable]: cannot find group with ${groupId} id`);
-            return;
-        }
-
-        if (group.touched) {
-            FiltersService.enableGroup(groupId);
-            return;
-        }
-
-        // If this is the first time the group has been activated - load and
-        // enable the recommended filters.
-        const recommendedFiltersIds = Categories.getRecommendedFilterIdsByGroupId(groupId);
-
-        FiltersService.enableGroup(groupId, recommendedFiltersIds);
-
-        return recommendedFiltersIds;
-    }
-
-    /**
-     * Called at the request to disable group of filters.
-     *
-     * @param message Message of {@link DisableFiltersGroupMessage} with group
-     * id to disable.
-     */
-    private static async onGroupDisable(message: DisableFiltersGroupMessage): Promise<void> {
-        const { groupId } = message.data;
-
-        logger.trace(`[ext.FiltersService.onGroupDisable]: background received message to disable group: id='${groupId}', name='${Categories.getGroupName(groupId)}'`);
-
-        Categories.disableGroup(groupId);
-
-        engine.debounceUpdate();
     }
 
     /**
@@ -236,36 +86,57 @@ export class FiltersService {
     }
 
     /**
-     * Called on a request to reset the counters of blocked ads.
+     * Enables specified group and updates filter engine.
+     *
+     * NOTE: we do not await of async task execution and returns array of recommended filters optimistic.
+     * TODO (v.zhelvis): handle enabling of recommended filters on frontend instead using this handler,
+     * because this is UI edge case.
+     *
+     * @param filterId Id of filter.
+     * @param shouldEnableGroup Flag for enabling the filter group if it has not been touched before.
      */
-    private static async resetBlockedAdsCount(): Promise<void> {
-        await PageStatsApi.reset();
+    protected static override async enableFilter(filterId: number, shouldEnableGroup = false): Promise<void> {
+        FiltersApi.loadAndEnableFilters([filterId], true, shouldEnableGroup);
     }
 
     /**
-     * Called on a request to add filter ids to consented filters list.
+     * Enables group on {@link EnableFiltersGroupMessage} message via {@link FiltersService.enableGroup}.
      *
-     * @param message Message of {@link SetConsentedFiltersMessage} with filter ids to add.
+     * If group is activated first time, provides list of recommended filters.
+     * NOTE: we do not await of async task execution and returns array of recommended filters optimistic.
+     * TODO (v.zhelvis): handle enabling of recommended filters on frontend instead using this handler,
+     * because this is UI edge case.
+     *
+     * @param message {@link EnableFiltersGroupMessage} With specified group id.
+     *
+     * @returns Array of recommended filters on first group activation.
      */
-    private static async setConsentedFilters(message: SetConsentedFiltersMessage): Promise<void> {
-        const { filterIds } = message.data;
+    protected static override async onGroupEnable(message: EnableFiltersGroupMessage): Promise<number[] | undefined> {
+        const { groupId } = message.data;
 
-        await annoyancesConsent.addFilterIds(filterIds);
-    }
+        const group = Categories.getGroupState(groupId);
 
-    /**
-     * Called on a request to check if filter is consented.
-     *
-     * @param message Message of {@link GetIsConsentedFilterMessage} with filter id to check.
-     *
-     * @returns True if consent is granted for filter, otherwise false.
-     */
-    private static async getIsConsentedFilter(message: GetIsConsentedFilterMessage): Promise<boolean> {
-        const { filterId } = message.data;
+        logger.trace(`[ext.FiltersService.onGroupEnable]: background received message to enable group: id='${groupId}', name='${Categories.getGroupName(groupId)}'`);
 
-        const isConsented = await annoyancesConsent.isConsentedFilter(filterId);
+        if (!group) {
+            logger.error(`[ext.FiltersService.onGroupEnable]: cannot find group with ${groupId} id`);
+            return;
+        }
 
-        return isConsented;
+        if (group.touched) {
+            // Fire-and-forget: no await to avoid blocking the response
+            this.enableGroup(groupId, []);
+            return;
+        }
+
+        // If this is the first time the group has been activated - load and
+        // enable the recommended filters.
+        const recommendedFiltersIds = Categories.getRecommendedFilterIdsByGroupId(groupId);
+
+        // Fire-and-forget: no await to avoid blocking the response
+        this.enableGroup(groupId, recommendedFiltersIds);
+
+        return recommendedFiltersIds;
     }
 
     /**
@@ -285,15 +156,9 @@ export class FiltersService {
     }
 
     /**
-     * Loads and enables specified filter.
-     * If filter group has not been touched before, it will be activated.
-     *
-     * Note: this method **does not update the engine**.
-     *
-     * @param filterId Id of filter.
-     * @param shouldEnableGroup Flag for enabling the filter group if it has not been touched before.
+     * @inheritdoc
      */
-    private static async enableFilter(filterId: number, shouldEnableGroup = false): Promise<void> {
-        await FiltersApi.loadAndEnableFilters([filterId], true, shouldEnableGroup);
+    protected static override async updateEngine(): Promise<void> {
+        engine.debounceUpdate();
     }
 }
