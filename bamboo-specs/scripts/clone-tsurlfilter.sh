0e6bd1213b2a44bd4d167cb22a81434588725458
Maxim Topciu <mtopciu@adguard.com>
2026-04-28 11:38:48 +0300

    AG-265 use --adguard-hit custom property for CSS hit stats

--- bamboo-specs/scripts/clone-tsurlfilter.sh
+++ bamboo-specs/scripts/clone-tsurlfilter.sh
@@ -30,7 +30,7 @@ exec 2>&1
 #   TSURLFILTER_REF="v2.1.0"           # tag
 #   TSURLFILTER_REF=""                 # skip cloning
 
-TSURLFILTER_REF=""
+TSURLFILTER_REF="fix/AG-265"
 
 # Repository URL
 # This should be set as a Bamboo project variable: tsurlfilterRepoUrl
