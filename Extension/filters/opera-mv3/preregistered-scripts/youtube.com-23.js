831c0fc2b9985b6a5d3d8dd158519f39d0f4b827
Alexandr Zaitsev <a.zaitsev@adguard.com>
2026-07-01 06:18:18 +0700

    update preregestry sources for opera

---
+++ Extension/filters/opera-mv3/preregistered-scripts/youtube.com-23.js
@@ -0,0 +1,5 @@
+(function () {
+var _g = window._g;
+if (!_g) return;
+_g.r("remove-class", {"name":"remove-class","args":["active-brand","body"],"engine":"extension","version":"2.4.2","verbose":false}, ["active-brand","body"], "f4dfc7cfcee3c87948d38a4d7d2404d1");
+})();
