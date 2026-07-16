047c9c2eecb0c36a7613387943ad27839769841c
Alexandr Zaitsev <a.zaitsev@adguard.com>
2026-07-16 15:36:18 +0700

    remove old preregestry build

--- Extension/filters/chromium-mv3/preregistered-scripts/youtube.com-19.js
+++
@@ -1,6 +0,0 @@
-(function () {
-var _g = window._g;
-if (!_g) return;
-_g.r("json-prune", {"name":"json-prune","args":["playerResponse.messages.[].youThereRenderer messages.[].youThereRenderer"],"engine":"extension","version":"2.4.2","verbose":false}, ["playerResponse.messages.[].youThereRenderer messages.[].youThereRenderer"], "470ee90d2301326ec3789ce34b41b3be");
-_g.r("json-prune", {"name":"json-prune","args":["overlay.bottomSheetOverlayRenderer","overlay.bottomSheetOverlayRenderer.displayImmediately"],"engine":"extension","version":"2.4.2","verbose":false}, ["overlay.bottomSheetOverlayRenderer","overlay.bottomSheetOverlayRenderer.displayImmediately"], "d0f891fdae741a62191ebec09a106f45");
-})();
